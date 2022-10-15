367773581b920d63fbe61e91f410c9a6129845ff
imadha <ibu@zipper.works>
2022-10-14 20:18:55 -0500

    Fix header look

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@ import {
   Avatar,
   Box,
   ButtonGroup,
-  Container,
-  Flex,
+  GridItem,
   HStack,
   IconButton,
   Link,
+  Text,
   useBreakpointValue,
   useColorModeValue,
 } from '@chakra-ui/react';
@@ -18,64 +18,70 @@ import {
   FiBell,
   FiChevronRight,
 } from 'react-icons/fi';
+import DefaultGrid from './default-grid';
 import { ZipperLogo } from './svg/zipper-logo';
 
 export default function Header() {
   const isDesktop = useBreakpointValue({ base: false, lg: true });
   const shadowValue = useColorModeValue('sm', 'sm-dark');
   return (
-    <Box as="header" pb={{ base: '10' }} px="100">
-      <Box as="nav" bg="bg-surface" boxShadow={shadowValue}>
-        <Box maxW="none" py="5">
-          <Flex justify="space-between">
-            <HStack spacing="4">
-              <Box h="6">
-                <Link href="/">
-                  <ZipperLogo style={{ maxHeight: '100%' }} />
-                </Link>
-              </Box>
-              <div>
-                <FiChevronRight />
-              </div>
-              <div>Zlack</div>
-              <Box px="8" />
-              <Link>Gallery</Link>
-              <Link>Learn</Link>
-              <Link>Build</Link>
-            </HStack>
-            <HStack spacing="4"></HStack>
-            {isDesktop ? (
-              <HStack spacing="4">
-                <ButtonGroup variant="ghost" spacing="1">
-                  <IconButton
-                    icon={<FiSearch fontSize="1.25rem" />}
-                    aria-label="Search"
-                  />
-                  <IconButton
-                    icon={<FiBell fontSize="1.25rem" />}
-                    aria-label="Notifications"
-                  />
-                  <IconButton
-                    icon={<FiHelpCircle fontSize="1.25rem" />}
-                    aria-label="Help Center"
-                  />
-                </ButtonGroup>
-                <Avatar
-                  boxSize="10"
-                  name="Yan Y Khan"
-                  src="https://avatars.githubusercontent.com/u/88688590?s=64"
-                />
-              </HStack>
-            ) : (
+    <DefaultGrid as="header" paddingY={12}>
+      <GridItem colSpan={3}>
+        <Link href="/" style={{ textDecoration: 'none' }}>
+          <HStack spacing={3} height="100%">
+            <Box height="4">
+              <ZipperLogo style={{ maxHeight: '100%' }} />
+            </Box>
+            <Box fontWeight="bold" fontSize="xl">
+              Functions
+            </Box>
+          </HStack>
+        </Link>
+      </GridItem>
+      <GridItem colSpan={3} as="nav">
+        <HStack
+          height="100%"
+          spacing={4}
+          fontSize="lg"
+          color="purple"
+          textDecoration="none"
+        >
+          <Link>Gallery</Link>
+          <Link>Learn</Link>
+          <Link>Build</Link>
+        </HStack>
+      </GridItem>
+      <GridItem colSpan={6} as="nav">
+        {isDesktop ? (
+          <HStack spacing="4" justifyContent="end">
+            <ButtonGroup variant="ghost" spacing="1">
               <IconButton
-                variant="ghost"
-                icon={<FiMenu fontSize="1.25rem" />}
-                aria-label="Open Menu"
+                icon={<FiSearch fontSize="1.25rem" />}
+                aria-label="Search"
               />
-            )}
-          </Flex>
-        </Box>
-      </Box>
-    </Box>
+              <IconButton
+                icon={<FiBell fontSize="1.25rem" />}
+                aria-label="Notifications"
+              />
+              <IconButton
+                icon={<FiHelpCircle fontSize="1.25rem" />}
+                aria-label="Help Center"
+              />
+            </ButtonGroup>
+            <Avatar
+              boxSize="10"
+              name="Yan Y Khan"
+              src="https://avatars.githubusercontent.com/u/88688590?s=64"
+            />
+          </HStack>
+        ) : (
+          <IconButton
+            variant="ghost"
+            icon={<FiMenu fontSize="1.25rem" />}
+            aria-label="Open Menu"
+          />
+        )}
+      </GridItem>
+    </DefaultGrid>
   );
 }
