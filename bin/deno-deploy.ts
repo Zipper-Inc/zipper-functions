003f4efafc254c81cf1089321f1f17e8d0985cd3
imadha <ibu@zipper.works>
2022-11-22 11:28:32 -0600

    Fix deploy script

--- bin/deno-deploy.ts
+++ bin/deno-deploy.ts
@@ -1,12 +1,10 @@
 import signale from 'signale';
 import shell, { exec } from '../src/utils/shell';
 import noop from '../src/utils/noop';
-import * as dotenv from 'dotenv';
 import { dirname, basename } from 'path';
 
-dotenv.config();
-
 const getFileFromArgs = () =>
+  // the file should always be the arg right after `deno:deploy`
   process.argv.find((_, i) => process.argv[i - 1]?.includes('/deno-deploy.ts'));
 
 async function main() {
@@ -47,29 +45,40 @@ async function main() {
 
   if (hasDeployCtl) signale.success('Deno Deploy CLI installed');
 
-  // forward args to deployctl
   const deployCtlArgs = process.argv.slice(
     process.argv.findIndex((_, i) =>
       process.argv[i - 1]?.includes('/deno-deploy.ts'),
     ) + 1,
   );
 
-  if (!deployCtlArgs.find((arg) => arg.startsWith('--token='))) {
-    deployCtlArgs.push(`--token=${process.env.DENO_DEPLOY_TOKEN}`);
+  if (!deployCtlArgs.find((arg) => arg.startsWith('token='))) {
+    deployCtlArgs.push(`token=${process.env.DENO_DEPLOY_TOKEN}`);
   }
 
-  if (!deployCtlArgs.find((arg) => arg.startsWith('--project='))) {
+  if (!deployCtlArgs.find((arg) => arg.startsWith('project='))) {
     const [name] = baseName.split('.');
-    deployCtlArgs.push(`--project=z-${name}`);
+    deployCtlArgs.push(`project=z-${name}`);
   }
 
-  if (!deployCtlArgs.find((arg) => arg.startsWith('--include='))) {
-    deployCtlArgs.push(`--include=${baseName}`);
+  // make sure not to upload any other files as static files, so we only include the one file
+  if (!deployCtlArgs.find((arg) => arg.startsWith('include='))) {
+    deployCtlArgs.push(`include=${baseName}`);
   }
 
-  await shell('deployctl', ['deploy', ...deployCtlArgs, baseName], {
-    cwd: dirname(file),
-  });
+  await shell(
+    'deployctl',
+    [
+      'deploy',
+      // Take the args and a `--` to them so they are compatible with `deployctl`
+      // see here https://deno.com/deploy/docs/deployctl
+      ...deployCtlArgs.map((arg) => `--${arg}`),
+      baseName,
+    ],
+    {
+      // run the deploy command from where the file lives
+      cwd: dirname(file),
+    },
+  );
 }
 
 main();
