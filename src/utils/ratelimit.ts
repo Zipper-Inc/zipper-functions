a2fe9619252e129c37d945d3706075d8315532f7
Sachin Ranchod <sachin.ranchod@gmail.com>
2022-12-06 15:09:13 -0800

    remove HMAC code from subhosting relay

--- src/utils/ratelimit.ts
+++ src/utils/ratelimit.ts
@@ -23,11 +23,11 @@ export default function rateLimit(options?: Options) {
 
         const currentUsage = tokenCount[0];
 
-        const isRateLimited = currentUsage >= limit;
+        const isRateLimited = currentUsage || 1 >= limit;
         res.setHeader('X-RateLimit-Limit', limit);
         res.setHeader(
           'X-RateLimit-Remaining',
-          isRateLimited ? 0 : limit - currentUsage,
+          isRateLimited ? 0 : limit - (currentUsage || 1),
         );
 
         return isRateLimited ? reject() : resolve();
