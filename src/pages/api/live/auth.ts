837ad12a5120506bead5d7b008d016ce5f978955
imadha <ibu@zipper.works>
2022-12-13 16:19:41 -0600

    f

--- src/pages/api/live/auth.ts
+++ src/pages/api/live/auth.ts
@@ -1,7 +1,6 @@
 import { authorize } from '@liveblocks/node';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { verifyAPIAuth } from '~/utils/verifyAuth';
-import { randomUUID } from 'crypto';
 
 const { LIVEBLOCKS_SECRET_KEY } = process.env;
 
@@ -18,16 +17,15 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     userId = await verifyAPIAuth(req, res);
   } catch (e) {
     // Handle anonymous public users
-    userId = `anon-${randomUUID()}`;
+    /**
+     * @todo check if app is pif (app?.isPublic) userId = `anon-${randomUUID()}`;
+     */
+    userId = null;
   }
 
-  /**
-   * @todo important: actually make sure they have access to the resource
-   * 
   if (!userId) {
-      return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(401).send({ error: 'Unauthorized' });
   }
-  */
 
   const response = await authorize({
     room: req.body.room,
