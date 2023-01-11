3e5b3f184b0c9dfd4637805fb0b0b6300e77abfc
Sachin Ranchod <sachin.ranchod@gmail.com>
2023-01-11 11:29:24 -0600

    fix playground; show dashboard if you're signed in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,149 +1,20 @@
-import { trpc } from '../utils/trpc';
+import { useUser } from '@clerk/nextjs';
+import { Dashboard } from '~/components/dashboard';
+import { Gallery } from '~/components/gallery';
 import { NextPageWithLayout } from './_app';
-import NextLink from 'next/link';
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Grid as InnerGrid,
-  GridItem,
-  Heading,
-  HStack,
-  Input,
-  Link,
-  VStack,
-} from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
-import React from 'react';
-import DefaultGrid from '~/components/default-grid';
-import { HiSparkles } from 'react-icons/hi';
-import { useRouter } from 'next/router';
-import { appSubmissionState } from '~/types/appSubmissionState';
-import { SignedIn, useOrganization } from '@clerk/nextjs';
 
 const IndexPage: NextPageWithLayout = () => {
-  const utils = trpc.useContext();
-  const router = useRouter();
-  const appQuery = trpc.useQuery([
-    'app.all',
-    { submissionState: appSubmissionState.approved },
-  ]);
-  const addApp = trpc.useMutation('app.add', {
-    async onSuccess() {
-      // refetches posts after a post is added
-      await utils.invalidateQueries(['app.all']);
-    },
-  });
-  const { register, handleSubmit } = useForm();
-  const { organization } = useOrganization();
-  // prefetch all posts for instant navigation
-  // useEffect(() => {
-  //   for (const { id } of postsQuery.data ?? []) {
-  //     utils.prefetchQuery(['post.byId', { id }]);
-  //   }
-  // }, [postsQuery.data, utils]);
+  const { user, isLoaded } = useUser();
 
-  return (
-    <DefaultGrid>
-      <GridItem colSpan={3}>
-        <VStack alignItems="start" spacing={2} w={280}>
-          <SignedIn>
-            <HStack>
-              <HiSparkles />
-              <Link fontWeight={600} onClick={() => router.push('/mine')}>
-                My apps
-              </Link>
-            </HStack>
-          </SignedIn>
-          <Link href="#">Popular Apps</Link>
-          <Link href="#">AI Magic</Link>
-          <Link href="#">Note Taking</Link>
-        </VStack>
-      </GridItem>
-      <GridItem colSpan={9}>
-        <InnerGrid
-          templateColumns="repeat(3, 280px)"
-          gridGap={10}
-          marginLeft={-4}
-        >
-          {appQuery.data?.map((app, index) => {
-            if (index === 0) {
-              return (
-                <GridItem
-                  colSpan={3}
-                  key={app.id}
-                  background="gray.100"
-                  borderRadius={10}
-                  p={4}
-                  height="240"
-                >
-                  <Heading as="h3" size="md">
-                    {app.name || app.slug}
-                  </Heading>
-                  <NextLink href={`/app/${app.id}`}>
-                    <Link textColor="brandPurple">View more</Link>
-                  </NextLink>
-                </GridItem>
-              );
-            }
+  if (user) {
+    return <Dashboard />;
+  }
 
-            return (
-              <GridItem
-                key={app.id}
-                height={240}
-                background="gray.100"
-                borderRadius={10}
-                p={4}
-              >
-                <Heading as="h3" size="md">
-                  {app.name || app.slug}
-                </Heading>
-                <NextLink href={`/app/${app.id}`}>
-                  <Link textColor="brandPurple">View more</Link>
-                </NextLink>
-              </GridItem>
-            );
-          })}
-          <GridItem>
-            <form
-              onSubmit={handleSubmit(({ slug }) => {
-                addApp.mutateAsync({ slug });
-              })}
-            >
-              <Flex marginTop="4" direction="column">
-                {addApp.error && (
-                  <FormErrorMessage>{addApp.error.message}</FormErrorMessage>
-                )}
-                <VStack alignItems="start">
-                  <Heading size="md">Create App</Heading>
-                  <FormControl>
-                    <FormLabel>Name:</FormLabel>
-                    <Input
-                      size="md"
-                      type="text"
-                      disabled={addApp.isLoading}
-                      {...register('slug')}
-                    />
-                  </FormControl>
-                  <Button
-                    type="submit"
-                    paddingX={6}
-                    disabled={addApp.isLoading}
-                    bgColor="purple.800"
-                    textColor="gray.100"
-                  >
-                    Submit
-                  </Button>
-                </VStack>
-              </Flex>
-            </form>
-          </GridItem>
-        </InnerGrid>
-      </GridItem>
-    </DefaultGrid>
-  );
+  if (isLoaded && !user) {
+    return <Gallery />;
+  }
+
+  return <div></div>;
 };
 
 IndexPage.skipAuth = true;
