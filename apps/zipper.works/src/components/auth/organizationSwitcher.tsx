47560a6983eb2e08c6d00575dfcb9e9f14c7b9c1
Sachin Ranchod <sachin@zipper.works>
2023-02-02 11:19:23 -0800

    add org create form to switcher

--- apps/zipper.works/src/components/auth/organizationSwitcher.tsx
+++ apps/zipper.works/src/components/auth/organizationSwitcher.tsx
@@ -11,21 +11,45 @@ import {
   Icon,
   VStack,
   IconButton,
-  Avatar,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  Input,
+  FormLabel,
+  FormHelperText,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { HiOutlineChevronUpDown, HiPlus } from 'react-icons/hi2';
+import {
+  HiBuildingStorefront,
+  HiOutlineChevronUpDown,
+  HiPlus,
+} from 'react-icons/hi2';
 import { HiSwitchHorizontal, HiCog } from 'react-icons/hi';
-import { useState } from 'react';
+import { FormEventHandler, useState } from 'react';
+import slugify from '~/utils/slugify';
 
 export const OrganizationSwitcher = () => {
   const { setActive, organizationList, isLoaded } = useOrganizationList();
   const { organization, membership } = useOrganization();
   const router = useRouter();
+  const {
+    isOpen: isOpenCreateOrg,
+    onOpen: onOpenCreateOrg,
+    onClose: onCloseCreateOrg,
+  } = useDisclosure();
 
   const [hoverOrg, setHoverOrg] = useState<string | undefined | null>(
     undefined,
   );
+
+  const { createOrganization } = useOrganizationList();
+  const [organizationName, setOrganizationName] = useState('');
 
   if (!isLoaded) return <></>;
 
@@ -38,90 +62,151 @@ export const OrganizationSwitcher = () => {
     return o.organization.id !== (organization?.id || null);
   });
 
+  const handleCreateOrgSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!createOrganization) return;
+    createOrganization({ name: organizationName });
+    setOrganizationName('');
+  };
+
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        rightIcon={<HiOutlineChevronUpDown />}
-        backgroundColor="transparent"
-      >
-        <Text fontSize="sm" fontWeight="medium">
-          {organization?.name || 'Personal Workspace'}
-        </Text>
-      </MenuButton>
-      <MenuList p={0} fontSize="sm">
-        <HStack borderBottom="1px" borderColor="gray.300" p={4} w="full">
-          <VStack flexGrow="1" alignItems="start" spacing={0}>
-            <Text fontWeight="medium">
-              {organization?.name || 'Personal Workspace'}
-            </Text>
-            {membership && <Text>{membership.role}</Text>}
-          </VStack>
-          {organization && (
-            <IconButton aria-label="Manage organization" variant={'ghost'}>
-              <HiCog />
-            </IconButton>
-          )}
-        </HStack>
-        {otherOrganizations.length > 0 && (
-          <Box
-            w="full"
-            backgroundColor={'gray.100'}
-            pl="4"
-            pt="4"
-            fontSize="xs"
-          >
-            <Text>Other organizations:</Text>
-          </Box>
-        )}
-        {otherOrganizations.map((org) => {
-          return (
-            <MenuItem
-              key={org.organization.id}
-              onClick={() => {
-                setActive && setActive({ organization: org.organization.id });
-                router.push(`${router.pathname}?reload=true`);
-              }}
-              backgroundColor="gray.100"
-              p="4"
-            >
-              <Box w="full">
-                <HStack>
-                  <Text
-                    w="full"
-                    fontWeight="medium"
-                    flexGrow={1}
-                    onMouseEnter={() => setHoverOrg(org.organization.id)}
-                    onMouseLeave={() => setHoverOrg(undefined)}
-                  >
-                    {org.organization.name}
-                  </Text>
-                  <Icon
-                    as={HiSwitchHorizontal}
-                    color={'gray.400'}
-                    visibility={
-                      hoverOrg === org.organization.id ? 'visible' : 'hidden'
-                    }
-                  ></Icon>
-                </HStack>
-              </Box>
-            </MenuItem>
-          );
-        })}
-        <MenuItem
-          backgroundColor="gray.100"
-          color="gray.600"
-          pt={2}
-          pb={4}
-          px={4}
+    <>
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={<HiOutlineChevronUpDown />}
+          backgroundColor="transparent"
         >
-          <HStack>
-            <Icon as={HiPlus}></Icon>
-            <Text>Create Organization</Text>
+          <Text fontSize="sm" fontWeight="medium">
+            {organization?.name || 'Personal Workspace'}
+          </Text>
+        </MenuButton>
+        <MenuList p={0} fontSize="sm">
+          <HStack borderBottom="1px" borderColor="gray.300" p={4} w="full">
+            <VStack flexGrow="1" alignItems="start" spacing={0}>
+              <Text fontWeight="medium">
+                {organization?.name || 'Personal Workspace'}
+              </Text>
+              {membership && <Text>{membership.role}</Text>}
+            </VStack>
+            {organization && (
+              <IconButton aria-label="Manage organization" variant={'ghost'}>
+                <HiCog />
+              </IconButton>
+            )}
           </HStack>
-        </MenuItem>
-      </MenuList>
-    </Menu>
+          {otherOrganizations.length > 0 && (
+            <Box
+              w="full"
+              backgroundColor={'gray.100'}
+              pl="4"
+              pt="4"
+              fontSize="xs"
+            >
+              <Text>Switch workspace:</Text>
+            </Box>
+          )}
+          {otherOrganizations.map((org) => {
+            return (
+              <MenuItem
+                key={org.organization.id}
+                onClick={() => {
+                  setActive && setActive({ organization: org.organization.id });
+                  router.push(`${router.pathname}?reload=true`);
+                }}
+                backgroundColor="gray.100"
+                px="4"
+                pt="2"
+              >
+                <Box w="full">
+                  <HStack>
+                    <Text
+                      w="full"
+                      fontWeight="medium"
+                      flexGrow={1}
+                      onMouseEnter={() => setHoverOrg(org.organization.id)}
+                      onMouseLeave={() => setHoverOrg(undefined)}
+                    >
+                      {org.organization.name}
+                    </Text>
+                    <Icon
+                      as={HiSwitchHorizontal}
+                      color={'gray.400'}
+                      visibility={
+                        hoverOrg === org.organization.id ? 'visible' : 'hidden'
+                      }
+                    ></Icon>
+                  </HStack>
+                </Box>
+              </MenuItem>
+            );
+          })}
+          <MenuItem
+            backgroundColor="gray.100"
+            color="gray.600"
+            pt={2}
+            pb={4}
+            px={4}
+            onClick={onOpenCreateOrg}
+          >
+            <HStack>
+              <Icon as={HiPlus}></Icon>
+              <Text>Create Organization</Text>
+            </HStack>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+      <Modal isOpen={isOpenCreateOrg} onClose={onCloseCreateOrg}>
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleCreateOrgSubmit}>
+            <ModalHeader>Create an Organization</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <FormControl>
+                <FormLabel fontSize="sm">Organization account name</FormLabel>
+                <Input
+                  type="text"
+                  name="organizationName"
+                  value={organizationName}
+                  onChange={(e) => setOrganizationName(e.currentTarget.value)}
+                />
+                {organizationName.length > 0 &&
+                  slugify(organizationName).length > 2 && (
+                    <>
+                      <FormHelperText>
+                        This will be your account name on Zipper.
+                      </FormHelperText>
+                      <FormHelperText>{`The url for your organization will be: ${
+                        process.env.NEXT_PUBLIC_HOST
+                      }/${slugify(organizationName)}`}</FormHelperText>
+                    </>
+                  )}
+
+                {organizationName.length > 0 &&
+                  slugify(organizationName).length < 3 && (
+                    <>
+                      <FormHelperText>
+                        The name must contain at least 3 alphanumeric
+                        characters.
+                      </FormHelperText>
+                    </>
+                  )}
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button variant="ghost" mr={3} onClick={onCloseCreateOrg}>
+                Close
+              </Button>
+              <Button colorScheme="purple" type="submit">
+                Create
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
