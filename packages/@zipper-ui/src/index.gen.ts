dc8e3b635e6c566cfeaa5a517d7367de9551760e
imadha <ibu@zipper.works>
2023-02-08 14:27:01 -0600

    Fix generated types

--- packages/@zipper-ui/src/index.gen.ts
+++
@@ -1,8 +0,0 @@
-// created from ctix 2023-02-06 20:05:23
-
-export * from './components/input-params-form';
-export * from './components/zipper-logo';
-export * from './hocs/with-chakra-provider';
-export * from './theme';
-export * from './utils/safe-json';
-export * from './utils/url';
