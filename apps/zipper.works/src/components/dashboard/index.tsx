c04deb62702c6e2f0d8046f3549d3a6726bd0b2f
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-02-10 17:26:59 +0300

    used react-table to manage apps table

--- apps/zipper.works/src/components/dashboard/index.tsx
+++ apps/zipper.works/src/components/dashboard/index.tsx
@@ -1,27 +1,95 @@
 import { inferQueryOutput, trpc } from '~/utils/trpc';
 import {
   GridItem,
-  Table,
   TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
+  Text,
   VStack,
   Heading,
   Button,
   Flex,
   Icon,
   Input,
+  HStack,
+  Link,
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import DefaultGrid from '~/components/default-grid';
 import { useOrganization, useOrganizationList, useUser } from '@clerk/nextjs';
-import { DashboardAppTableRows } from './app-table-rows';
 import { CreateAppModal } from './create-app-modal';
 
 import { FiPlus } from 'react-icons/fi';
+import { DataTable } from './table';
+import { createColumnHelper } from '@tanstack/react-table';
+import { LockIcon, UnlockIcon } from '@chakra-ui/icons';
+import router from 'next/router';
+
+type AppOwner = { name: string; type: 'user' | 'org' };
+type _App = Unpack<inferQueryOutput<'app.byAuthedUser'>>;
+type App = _App & {
+  owner: AppOwner;
+  updatedAt: Date;
+};
+
+const prepareAppsData = (
+  apps: _App[],
+  getAppOwner: (app: _App) => AppOwner,
+) => {
+  return apps.map((app): App => {
+    const { createdAt, updatedAt, name, slug } = app;
+    const lastUpdatedAt = updatedAt || createdAt;
+    const owner = getAppOwner(app);
+
+    return {
+      ...app,
+      updatedAt: lastUpdatedAt,
+      name: name || slug,
+      owner,
+    };
+  });
+};
+
+const columnHelper = createColumnHelper<App>();
+
+const columns = [
+  columnHelper.accessor('name', {
+    cell: ({
+      getValue,
+      row: {
+        original: { isPrivate, resourceOwner, slug, description },
+      },
+    }) => (
+      <VStack align={'start'} py="2">
+        <HStack>
+          {isPrivate ? <LockIcon /> : <UnlockIcon />}
+          <Link
+            fontSize={'md'}
+            fontWeight={600}
+            onClick={() =>
+              router.push(`/${resourceOwner.slug}/${slug}/edit/main.ts`)
+            }
+          >
+            {getValue()}
+          </Link>
+        </HStack>
+        <Text>{description}</Text>
+      </VStack>
+    ),
+    header: 'App Name',
+  }),
+  columnHelper.accessor('updatedAt', {
+    cell: (info) =>
+      new Intl.DateTimeFormat('en-GB', {
+        dateStyle: 'short',
+      }).format(info.getValue()),
+    header: 'Last Updated',
+  }),
+  columnHelper.accessor('owner.name', {
+    cell: (info) => info.getValue(),
+    header: 'Owner',
+  }),
+];
+
+const emptyApps: App[] = [];
 
 export function Dashboard() {
   const { user } = useUser();
@@ -32,17 +100,19 @@ export function Dashboard() {
   const [isCreateAppModalOpen, setCreateAppModalOpen] = useState(false);
   const { organizationList } = useOrganizationList();
 
-  const getAppOwner = (app: Unpack<inferQueryOutput<'app.byAuthedUser'>>) => {
+  const getAppOwner = (app: _App): AppOwner => {
     if (
       app.resourceOwner.resourceOwnerId === user?.id ||
       app.createdById === user?.id
     ) {
-      return 'You';
+      return { name: 'You', type: 'user' };
     }
-    return organizationList?.find(
-      ({ organization }) =>
-        organization.id === app.resourceOwner.resourceOwnerId,
-    )?.organization.name;
+    const orgName =
+      organizationList?.find(
+        ({ organization }) =>
+          organization.id === app.resourceOwner.resourceOwnerId,
+      )?.organization.name ?? '';
+    return { name: orgName, type: 'org' };
   };
 
   useEffect(() => {
@@ -66,6 +136,8 @@ export function Dashboard() {
       setDisplayedApps(appQuery.data);
     }
   }, [appSearchTerm, appQuery.data]);
+
+  const data = prepareAppsData(displayedApps ?? emptyApps, getAppOwner);
 
   return (
     <>
@@ -97,32 +169,11 @@ export function Dashboard() {
               onChange={(e) => setAppSearchTerm(e.target.value)}
             />
             <TableContainer w="full">
-              <Table size={'sm'}>
-                <Thead>
-                  <Tr>
-                    <Th>App Name</Th>
-                    <Th>Last Updated</Th>
-                    <Th>Owner</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {displayedApps && (
-                    <DashboardAppTableRows
-                      apps={displayedApps}
-                      getAppOwner={getAppOwner}
-                    />
-                  )}
-                  {!appQuery.isLoading && !appQuery.data && (
-                    <Tr>
-                      <Td>
-                        You're all out of apps. Probably a good time to create
-                        one.
-                      </Td>
-                      <Td />
-                    </Tr>
-                  )}
-                </Tbody>
-              </Table>
+              <DataTable
+                columns={columns}
+                data={data}
+                isEmpty={!appQuery.isLoading && !appQuery.data}
+              />
             </TableContainer>
           </VStack>
         </GridItem>
