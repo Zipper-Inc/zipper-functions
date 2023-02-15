8937d9779e77a16a296caca8de333c644cfd3b0c
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-02-16 00:40:23 +0300

    added skeleton for the app detail page

--- apps/zipper.works/src/pages/[resource-owner]/[app-slug]/index.tsx
+++ apps/zipper.works/src/pages/[resource-owner]/[app-slug]/index.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  GridItem,
   Heading,
   HStack,
   Link,
@@ -11,20 +10,26 @@ import {
   Tr,
   Text,
   VStack,
+  Box,
 } from '@chakra-ui/react';
 import { useUser } from '@clerk/nextjs';
+import Editor from '@monaco-editor/react';
 import { GetServerSideProps } from 'next';
 import NextError from 'next/error';
 import { useRouter } from 'next/router';
 import { HiArrowRight } from 'react-icons/hi';
-import DefaultGrid from '~/components/default-grid';
+import { EditorNav } from '~/components/editor/editor-nav';
 import Header from '~/components/header';
 import { NextPageWithLayout } from '~/pages/_app';
 import { getValidSubdomain, removeSubdomains } from '~/utils/subdomains';
 import { trpc } from '~/utils/trpc';
 
+const APPROXIMATE_HEADER_HEIGHT_PX = '116px';
+const CODE_PREVIEW_HEIGHT = `calc(100vh - ${APPROXIMATE_HEADER_HEIGHT_PX})`;
+
 const AppPage: NextPageWithLayout = () => {
   const router = useRouter();
+  const headline = 'Turn a meeting into a task list';
 
   const resourceOwnerSlug = router.query['resource-owner'] as string;
   const appSlug = router.query['app-slug'] as string;
@@ -61,97 +66,164 @@ const AppPage: NextPageWithLayout = () => {
 
   const { data } = appQuery;
   return (
-    <DefaultGrid mt="14">
-      <GridItem colSpan={5}>
-        <VStack gap={4} alignItems="start" w={560}>
-          <Heading as="h1" size="2xl" fontWeight="bold" overflowWrap="normal">
-            {data.name || data.slug}
-          </Heading>
+    <HStack gap={4} pl={10} mt="14" mr={0} alignItems="start">
+      <VStack flex={2} gap={4} alignItems="start" w={560}>
+        <Heading as="h1" size="2xl" fontWeight="bold" overflowWrap="normal">
+          {headline}
+        </Heading>
+        <Heading as="h2" color="purple.700" fontSize="2xl">
+          {data.name || data.slug}
+        </Heading>
 
-          <VStack spacing={4} align={'start'}>
-            {data.description && (
-              <>
-                <Text fontWeight={500} color="gray.500">
-                  About this app
-                </Text>
-                <Text fontSize="lg">{data.description}</Text>
-              </>
-            )}
-            {forksQuery.data && forksQuery.data.length > 0 && (
-              <>
-                <Text fontWeight={500} color="gray.500">
-                  Your forks
-                </Text>
-                <TableContainer
-                  border="1px solid"
-                  borderRadius={10}
-                  borderColor="gray.100"
-                >
-                  <Table>
-                    <Tbody>
-                      {forksQuery.data.map((fork) => (
-                        <Tr key={fork.id}>
-                          <Td>
-                            <HStack>
-                              <HiArrowRight />
-                              <Link
-                                onClick={() =>
-                                  router.push(
-                                    `/${fork.resourceOwner?.slug}/${fork.slug}/edit`,
-                                  )
-                                }
-                              >
-                                {fork.name || fork.slug}
-                              </Link>
-                            </HStack>
-                          </Td>
-                        </Tr>
-                      ))}
-                    </Tbody>
-                  </Table>
-                </TableContainer>
-              </>
-            )}
-
-            <HStack pt={10}>
-              <Button
-                colorScheme={'purple'}
-                variant={'outline'}
-                paddingX={6}
-                onClick={() => {
-                  if (user) {
-                    forkApp.mutateAsync({ id: appQuery.data.id });
-                  } else {
-                    router.push(
-                      `/sign-in?redirect=${removeSubdomains(
-                        window.location.host,
-                      )}/${window.location.pathname}}`,
-                    );
-                  }
-                }}
+        <VStack spacing={4} align={'start'} minW="full">
+          {data.description && (
+            <>
+              <Text fontWeight={700}>About this app</Text>
+              <Text fontSize="lg">{data.description}</Text>
+            </>
+          )}
+        </VStack>
+        <HStack alignSelf="stretch" alignItems="start" mt={5}>
+          <VStack alignItems="stretch" flex={1}>
+            <HStack>
+              <Text flex={1} color="gray.700">
+                Built by
+              </Text>
+              <Text flex={1} color="purple.600">
+                {resourceOwnerSlug}
+              </Text>
+            </HStack>
+            <HStack overflow="hidden" minWidth={0}>
+              <Text flex={1} color="gray.700">
+                Updated
+              </Text>
+              <Text
+                flex={1}
+                color="purple.600"
+                overflow="auto"
+                whiteSpace="nowrap"
               >
-                Fork
-              </Button>
-
-              <Button
-                colorScheme={'purple'}
-                paddingX={6}
-                variant={'outline'}
-                onClick={() => {
-                  router.push(
-                    `/${resourceOwnerSlug}/${appSlug}/edit/${
-                      appQuery.data.scriptMain?.script.filename || 'main.ts'
-                    }`,
-                  );
-                }}
-              >
-                View
-              </Button>
+                {new Intl.DateTimeFormat('en-GB', {
+                  dateStyle: 'short',
+                }).format(data?.updatedAt || undefined)}
+              </Text>
             </HStack>
           </VStack>
+          <HStack flex={1} alignItems="stretch">
+            {/* <Text color="gray.700" flex={1}>
+              Categories
+            </Text>
+            <VStack flex={2} alignItems="stretch">
+              <Text color="purple.600">Artifical Intelligence </Text>
+              <Text color="purple.600">Project Management</Text>
+              <Text color="purple.600"> Speech to Text</Text>
+            </VStack> */}
+          </HStack>
+        </HStack>
+
+        {forksQuery.data && forksQuery.data.length > 0 && (
+          <>
+            <Text fontWeight={500} color="gray.500">
+              Your forks
+            </Text>
+            <TableContainer
+              border="1px solid"
+              borderRadius={10}
+              borderColor="gray.100"
+            >
+              <Table>
+                <Tbody>
+                  {forksQuery.data.map((fork) => (
+                    <Tr key={fork.id}>
+                      <Td>
+                        <HStack>
+                          <HiArrowRight />
+                          <Link
+                            onClick={() =>
+                              router.push(
+                                `/${fork.resourceOwner?.slug}/${fork.slug}/edit`,
+                              )
+                            }
+                          >
+                            {fork.name || fork.slug}
+                          </Link>
+                        </HStack>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </>
+        )}
+
+        <HStack pt={10}>
+          <Button
+            colorScheme={'purple'}
+            variant={'outline'}
+            paddingX={6}
+            onClick={() => {
+              if (user) {
+                forkApp.mutateAsync({ id: appQuery.data.id });
+              } else {
+                router.push(
+                  `/sign-in?redirect=${removeSubdomains(
+                    window.location.host,
+                  )}/${window.location.pathname}}`,
+                );
+              }
+            }}
+          >
+            Fork
+          </Button>
+
+          <Button
+            colorScheme={'purple'}
+            paddingX={6}
+            variant={'outline'}
+            onClick={() => {
+              router.push(
+                `/${resourceOwnerSlug}/${appSlug}/edit/${
+                  appQuery.data.scriptMain?.script.filename || 'main.ts'
+                }`,
+              );
+            }}
+          >
+            View
+          </Button>
+        </HStack>
+      </VStack>
+      <HStack
+        flex={3}
+        overflow="hidden"
+        spacing={0}
+        height={CODE_PREVIEW_HEIGHT}
+        alignItems="stretch"
+      >
+        <VStack
+          flex={1}
+          background="linear-gradient(326.37deg, #3E1C96 8.28%, rgba(62, 28, 150, 0) 100.06%), #89279B;"
+          borderTopLeftRadius={12}
+          padding={6}
+          alignItems="stretch"
+          spacing={6}
+        >
+          <EditorNav app={data} />
         </VStack>
-      </GridItem>
-    </DefaultGrid>
+        <Box flex={2}>
+          <Editor
+            defaultLanguage="typescript"
+            defaultValue={data.scriptMain?.script.code}
+            theme="vs-light"
+            options={{
+              minimap: { enabled: false },
+              automaticLayout: true,
+              readOnly: true,
+            }}
+          />
+        </Box>
+      </HStack>
+    </HStack>
   );
 };
 
