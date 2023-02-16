debf36b4a7dc4eeab54d458dddd34b0b2c318b5f
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-02-16 19:28:57 +0300

    first app card

--- apps/zipper.works/src/components/gallery.tsx
+++ apps/zipper.works/src/components/gallery.tsx
@@ -62,20 +62,67 @@ export function Gallery({
             {apps.map((app, index) => {
               if (index === 0) {
                 return (
-                  <GridItem
-                    colSpan={3}
-                    key={app.id}
-                    background="gray.100"
-                    borderRadius={10}
-                    p={4}
-                    height="240"
-                  >
-                    <Heading as="h2" size="md">
+                  <GridItem colSpan={3} key={app.id} width="full">
+                    <NextLink href={`/${app.resourceOwner.slug}/${app.id}`}>
+                      <Card
+                        width="full"
+                        background="gray.100"
+                        borderRadius={10}
+                      >
+                        <CardBody
+                          padding={0}
+                          as={HStack}
+                          flexDirection="row-reverse"
+                          alignItems="start"
+                          justifyContent="space-between"
+                          height="full"
+                          paddingBottom={6}
+                        >
+                          {/* TODO get a generated image */}
+                          <Box flex={1}>
+                            <Image
+                              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                              alt="Green double couch with wooden legs"
+                            />
+                          </Box>
+                          <Stack spacing="1" paddingX={4} flex={1}>
+                            <Heading
+                              as="h3"
+                              size="sm"
+                              color="gray.800"
+                              fontWeight="normal"
+                            >
+                              {app.resourceOwner.slug}
+                            </Heading>
+                            <Heading as="h2" size="md">
+                              {app.name || app.slug}
+                            </Heading>
+                            <VStack alignItems="stretch">
+                              {badges.length > 0 && (
+                                <HStack paddingY={1}>
+                                  {badges.map((badge) => (
+                                    <Badge
+                                      colorScheme="blackAlpha"
+                                      rounded="xl"
+                                      paddingX={2}
+                                      color="gray.600"
+                                    >
+                                      {badge}
+                                    </Badge>
+                                  ))}
+                                </HStack>
+                              )}
+                            </VStack>
+                          </Stack>
+                        </CardBody>
+                      </Card>
+                    </NextLink>
+                    {/* <Heading as="h2" size="md">
                       {app.name || app.slug}
                     </Heading>
                     <NextLink href={`/${app.resourceOwner.slug}/${app.slug}`}>
                       <Text textColor="brandPurple">View more</Text>
-                    </NextLink>
+                    </NextLink> */}
                   </GridItem>
                 );
               }
@@ -103,7 +150,7 @@ export function Gallery({
                           src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                           alt="Green double couch with wooden legs"
                         />
-                        <Stack spacing="1" paddingX={4}>
+                        <Stack spacing="1" width="full" paddingX={4}>
                           <Heading
                             as="h3"
                             size="sm"
