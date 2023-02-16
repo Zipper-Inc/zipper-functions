0c5efbfa7d9ac94b175cbc6ec338c19771e82d65
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-02-16 18:20:44 +0300

    skeleton gallery app card

--- apps/zipper.works/src/components/gallery.tsx
+++ apps/zipper.works/src/components/gallery.tsx
@@ -1,19 +1,27 @@
 import NextLink from 'next/link';
 import {
+  Badge,
   Box,
+  Card,
+  CardBody,
   Grid as InnerGrid,
   GridItem,
   Heading,
   HStack,
   Link,
+  Stack,
   Text,
   VStack,
+  Image,
 } from '@chakra-ui/react';
 import React from 'react';
 import DefaultGrid from '~/components/default-grid';
 import { HiSparkles } from 'react-icons/hi';
 import { SignedIn } from '@clerk/nextjs';
 import { GalleryAppQueryOutput } from '~/pages';
+
+// TODO get the badges from api
+const badges: string[] = [];
 
 export function Gallery({
   apps,
@@ -62,7 +70,7 @@ export function Gallery({
                     p={4}
                     height="240"
                   >
-                    <Heading as="h3" size="md">
+                    <Heading as="h2" size="md">
                       {app.name || app.slug}
                     </Heading>
                     <NextLink href={`/${app.resourceOwner.slug}/${app.slug}`}>
@@ -73,18 +81,59 @@ export function Gallery({
               }
 
               return (
-                <GridItem
-                  key={app.id}
-                  height={240}
-                  background="gray.100"
-                  borderRadius={10}
-                  p={4}
-                >
-                  <Heading as="h3" size="md">
-                    {app.name || app.slug}
-                  </Heading>
+                <GridItem key={app.id}>
                   <NextLink href={`/${app.resourceOwner.slug}/${app.id}`}>
-                    <Text textColor="brandPurple">View more</Text>
+                    <Card
+                      width="full"
+                      background="gray.100"
+                      height="full"
+                      overflow="hidden"
+                      borderRadius={10}
+                    >
+                      <CardBody
+                        padding={0}
+                        as={VStack}
+                        alignItems="start"
+                        justifyContent="space-between"
+                        height="full"
+                        paddingBottom={6}
+                      >
+                        {/* TODO get a generated image */}
+                        <Image
+                          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                          alt="Green double couch with wooden legs"
+                        />
+                        <Stack spacing="1" paddingX={4}>
+                          <Heading
+                            as="h3"
+                            size="sm"
+                            color="gray.800"
+                            fontWeight="normal"
+                          >
+                            {app.resourceOwner.slug}
+                          </Heading>
+                          <Heading as="h2" size="md">
+                            {app.name || app.slug}
+                          </Heading>
+                          <VStack alignItems="stretch">
+                            {badges.length > 0 && (
+                              <HStack paddingY={1}>
+                                {badges.map((badge) => (
+                                  <Badge
+                                    colorScheme="blackAlpha"
+                                    rounded="xl"
+                                    paddingX={2}
+                                    color="gray.600"
+                                  >
+                                    {badge}
+                                  </Badge>
+                                ))}
+                              </HStack>
+                            )}
+                          </VStack>
+                        </Stack>
+                      </CardBody>
+                    </Card>
                   </NextLink>
                 </GridItem>
               );
