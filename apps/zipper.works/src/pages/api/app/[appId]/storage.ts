a757fddc65b08f4de1773ceff88d55a92fa2e26b
Sachin Ranchod <sachin@zipper.works>
2023-02-16 18:38:58 -0600

    add storage API to .works

---
+++ apps/zipper.works/src/pages/api/app/[appId]/storage.ts
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '~/server/prisma';
+import { Prisma } from '@prisma/client';
+import { JSONObject } from 'superjson/dist/types';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { body, method, query } = req;
+  const { appId } = query;
+
+  const app = await prisma.app.findUniqueOrThrow({
+    where: { id: appId as string },
+    select: { datastore: true },
+  });
+
+  let datastore = app.datastore as JSONObject;
+
+  switch (method) {
+    case 'GET': {
+      const k = query.key as string;
+      if (k && datastore) {
+        res.send({ key: k, value: datastore[k] });
+        break;
+      }
+      res.send(datastore || {});
+      break;
+    }
+    case 'POST': {
+      if (body.key) {
+        datastore = datastore || {};
+        datastore[body.key] = body.value;
+
+        await prisma.app.update({
+          where: { id: appId as string },
+          data: { datastore: datastore as Prisma.InputJsonValue },
+        });
+
+        res.send({ key: body.key, value: datastore[body.key] });
+        break;
+      }
+      res.status(400).send({ error: 'Missing key' });
+      break;
+    }
+    case 'DELETE': {
+      if (body.key) {
+        delete datastore[body.key];
+        await prisma.app.update({
+          where: { id: appId as string },
+          data: { datastore: datastore as Prisma.InputJsonValue },
+        });
+        res.send(true);
+        break;
+      }
+      res.status(400).send({ error: 'Missing key' });
+      break;
+    }
+    default: {
+      res
+        .status(400)
+        .send({ error: 'Unsupported method: use GET, POST, or DELETE' });
+    }
+  }
+}
