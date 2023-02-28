d86e7c4c2be7e5067f032d0b07a089c76f1fba47
imadha <ibu@zipper.works>
2023-02-28 08:53:11 -0600

    Wrap output in error boundary with raw output backup

--- packages/@zipper-ui/src/components/function-output/function-output.tsx
+++ packages/@zipper-ui/src/components/function-output/function-output.tsx
@@ -2,26 +2,48 @@ import { Tab, Tabs, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
 import { Props } from './types';
 import { RawFunctionOutput } from './raw-function-output';
 import { SmartFunctionOutput } from './smart-function-output';
+import { ErrorBoundary } from '../error-boundary';
 
 export function FunctionOutput({ result, level = 0 }: Props) {
   return (
-    <Tabs colorScheme="purple" variant="enclosed">
-      <TabList>
-        <Tab>Result</Tab>
-        <Tab>Raw Output</Tab>
-      </TabList>
-      <TabPanels
-        border="1px solid"
-        borderColor="gray.200"
-        borderBottomRadius={'md'}
-      >
-        <TabPanel>
-          <SmartFunctionOutput result={result} level={level} />
-        </TabPanel>
-        <TabPanel backgroundColor="gray.100">
-          <RawFunctionOutput result={result} />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <ErrorBoundary
+      // this makes sure we render a new boundary with a new result set
+      key={JSON.stringify(result)}
+      fallback={
+        <Tabs colorScheme="purple" variant="enclosed">
+          <TabList>
+            <Tab>Raw Output</Tab>
+          </TabList>
+          <TabPanels
+            border="1px solid"
+            borderColor="gray.200"
+            borderBottomRadius={'md'}
+          >
+            <TabPanel backgroundColor="gray.100">
+              <RawFunctionOutput result={result} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      }
+    >
+      <Tabs colorScheme="purple" variant="enclosed">
+        <TabList>
+          <Tab>Result</Tab>
+          <Tab>Raw Output</Tab>
+        </TabList>
+        <TabPanels
+          border="1px solid"
+          borderColor="gray.200"
+          borderBottomRadius={'md'}
+        >
+          <TabPanel>
+            <SmartFunctionOutput result={result} level={level} />
+          </TabPanel>
+          <TabPanel backgroundColor="gray.100">
+            <RawFunctionOutput result={result} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </ErrorBoundary>
   );
 }
