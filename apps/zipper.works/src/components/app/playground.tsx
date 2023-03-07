2ee88a71dc4912b6589c87acba15f61678fa868a
fawz24 <fawzyabderrahman@gmail.com>
2023-03-07 17:34:46 +0300

    Playground redesign:  Tabs (#26)
    * extracting PlaygroundSidebarScriptItem
    
    * moved duplication logic to the PlaygroudnSidebar component
    
    * fixed and moved start file renaming logic to the PlaygroundSidebar
    
    * fixed file deletion logic and used chakra-ui role="group" for changing the css of hovered item
    
    * reloading scripts on duplicate/rename/delete
    
    * opening main script when currently viewed script is deleted
    
    * reloading scripts after custom script or connector created
    
    * extracted useScriptFilename hook
    
    * renamed EditorNav to EditorPreviewNav
    
    * styled the playground sidebar
    
    * restructuring playground layout using display:flex
    
    * extracted useAppEditors hook
    
    * extracted PlaygroundAvatars component to its own file
    
    * removed playground avatars and share button from header
    
    * styled playground tabs and added share button and avatars next to the tabs

--- apps/zipper.works/src/components/app/playground.tsx
+++ apps/zipper.works/src/components/app/playground.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   GridItem,
   HStack,
   Tabs,
@@ -8,8 +7,11 @@ import {
   TabPanels,
   TabPanel,
   Progress,
+  VStack,
+  Text,
+  ChakraProps,
+  Button,
 } from '@chakra-ui/react';
-
 import { InputParam } from '@zipper/types';
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
@@ -18,10 +20,7 @@ import SchedulesTab from '~/components/app/schedules-tab';
 import SettingsTab from './settings-tab';
 import ShareModal from '~/components/app/share-modal';
 import { trpc } from '~/utils/trpc';
-
 import { parseInputForTypes } from '~/utils/parse-input-for-types';
-
-import DefaultGrid from '../default-grid';
 import { PlaygroundHeader } from './playground-header';
 import { CodeTab } from './code-tab';
 import { useUser } from '@clerk/nextjs';
@@ -29,6 +28,26 @@ import { useEditorContext } from '../context/editor-context';
 import { Script } from '@prisma/client';
 import { AppQueryOutput } from '~/types/trpc';
 import { RunAppProvider } from '../context/run-app-context';
+import { PlaygroundAvatars } from './playground-avatars';
+import { useAppEditors } from '~/hooks/use-app-editors';
+import { HiOutlineUpload } from 'react-icons/hi';
+
+const tabStyles: ChakraProps = {
+  px: 6,
+  py: 3,
+  rounded: 'md',
+  _selected: {
+    backgroundColor: 'purple.50',
+    fontWeight: 'bold',
+    textColor: 'purple.700',
+    _hover: { transform: 'none' },
+  },
+  _hover: {
+    backgroundColor: 'purple.50',
+    textColor: 'purple.700',
+    transform: 'scale(1.05)',
+  },
+};
 
 export function Playground({
   app,
@@ -39,6 +58,7 @@ export function Playground({
 }) {
   const { user } = useUser();
   const router = useRouter();
+  const { editorIds, onlineEditorIds, selfId } = useAppEditors();
 
   const [inputParams, setInputParams] = useState<InputParam[]>([]);
   const [tabIndex, setTabIndex] = useState(0);
@@ -83,15 +103,9 @@ export function Playground({
       onBeforeRun={save}
       onAfterRun={switchToCodeTab}
     >
-      <Tabs
-        colorScheme="purple"
-        index={tabIndex}
-        onChange={(index) => setTabIndex(index)}
-        paddingX={10}
-      >
+      <VStack flex={1} paddingX={10} alignItems="stretch" spacing={0}>
         <PlaygroundHeader
           app={app}
-          onClickShare={() => setShareModalOpen(true)}
           onClickFork={() => {
             if (app.canUserEdit) return;
             if (user) {
@@ -101,85 +115,103 @@ export function Playground({
             }
           }}
         />
-
-        <TabList
-          as={HStack}
-          gap={2}
-          borderColor={tabIndex === 0 ? 'purple.100' : 'inherit'}
-          mt={3}
+        <Tabs
+          colorScheme="purple"
+          index={tabIndex}
+          onChange={(index) => setTabIndex(index)}
+          flex={1}
+          display="flex"
+          flexDirection="column"
+          justifyContent="start"
         >
-          {/* CODE */}
-          <Tab
-            backgroundColor="gray.200"
-            borderTopRadius="md"
-            _selected={{
-              borderColor: 'purple.500',
-              backgroundColor: 'purple.500',
-              fontWeight: 'bold',
-              textColor: 'white',
-            }}
+          <TabList
+            border="none"
+            mt={3}
+            color="gray.500"
+            gap={4}
+            justifyContent="space-between"
           >
-            <Box px="2">Code</Box>
-          </Tab>
-
-          {app.canUserEdit && (
-            <>
-              {/* SCHEDULES */}
-              <Tab>Schedules</Tab>
-              {/* SECRETS */}
-              <Tab>Secrets</Tab>
-            </>
-          )}
-          {/* SETTINGS */}
-          <Tab>Settings</Tab>
-        </TabList>
-        {/* TAB PANELS */}
-        <TabPanels as={DefaultGrid} maxW="full" p={0}>
-          {/* CODE */}
-          <TabPanel
-            as={GridItem}
-            colSpan={12}
-            px={0}
-            pb={0}
-            position="relative"
-          >
-            {isSaving && (
-              <Progress
-                isIndeterminate
-                colorScheme="purple"
-                position="absolute"
-                left={0}
-                right={0}
-                top="-3px"
-                height="2px"
-                background="transparent"
+            <HStack spacing={2}>
+              {/* CODE */}
+              <Tab {...tabStyles}>
+                <Text>Code</Text>
+              </Tab>
+              {app.canUserEdit && (
+                <>
+                  {/* SCHEDULES */}
+                  <Tab {...tabStyles}>Schedules</Tab>
+                  {/* SECRETS */}
+                  <Tab {...tabStyles}>Secrets</Tab>
+                  {/* SETTINGS */}
+                  <Tab {...tabStyles}>Settings</Tab>
+                </>
+              )}
+            </HStack>
+            <HStack justifySelf="start">
+              {app.canUserEdit && (
+                <Button
+                  colorScheme="purple"
+                  variant="ghost"
+                  onClick={() => setShareModalOpen(true)}
+                  display="flex"
+                  gap={2}
+                  fontWeight="medium"
+                >
+                  <HiOutlineUpload />
+                  <Text>Share</Text>
+                </Button>
+              )}
+              <PlaygroundAvatars
+                editorIds={editorIds}
+                onlineEditorIds={onlineEditorIds}
+                selfId={selfId}
               />
+            </HStack>
+          </TabList>
+          {/* TAB PANELS */}
+          <TabPanels as={VStack} alignItems="stretch" flex={1}>
+            {/* CODE */}
+            <TabPanel p={0} pt={9} position="relative" flex={1}>
+              {isSaving && (
+                <Progress
+                  isIndeterminate
+                  colorScheme="purple"
+                  position="absolute"
+                  left={0}
+                  right={0}
+                  top="-3px"
+                  height="2px"
+                  background="transparent"
+                />
+              )}
+              <CodeTab app={app} mainScript={mainScript} />
+            </TabPanel>
+
+            {/* SCHEDULES */}
+            <TabPanel as={GridItem} colSpan={12}>
+              <SchedulesTab inputParams={inputParams} appId={id} />
+            </TabPanel>
+
+            {/* SECRETS */}
+            <TabPanel as={GridItem} colSpan={12}>
+              <SecretsTab editable={app.canUserEdit} appId={id} />
+            </TabPanel>
+
+            {/* SETTINGS */}
+            {app.canUserEdit && (
+              <TabPanel as={GridItem} colSpan={12}>
+                <SettingsTab app={app} />
+              </TabPanel>
             )}
-            <CodeTab app={app} mainScript={mainScript} />
-          </TabPanel>
+          </TabPanels>
 
-          {/* SCHEDULES */}
-          <TabPanel as={GridItem} colSpan={12}>
-            <SchedulesTab inputParams={inputParams} appId={id} />
-          </TabPanel>
-
-          {/* SECRETS */}
-          <TabPanel as={GridItem} colSpan={12}>
-            <SecretsTab editable={app.canUserEdit} appId={id} />
-          </TabPanel>
-
-          {/* SETTINGS */}
-          <TabPanel as={GridItem} colSpan={12}>
-            <SettingsTab app={app} />
-          </TabPanel>
-        </TabPanels>
-
-        <ShareModal
-          isOpen={isShareModalOpen}
-          onClose={() => setShareModalOpen(false)}
-          appId={id}
-        />
-      </Tabs>
+          <ShareModal
+            isOpen={isShareModalOpen}
+            onClose={() => setShareModalOpen(false)}
+            appId={id}
+          />
+        </Tabs>
+      </VStack>
     </RunAppProvider>
   );
 }
