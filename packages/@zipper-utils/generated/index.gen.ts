701facf0c8eb526f81938a1950e2f4f6270351f5
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-03-31 05:51:57 +0300

    removed get-last-run-version.ts from @zipper-utils

--- packages/@zipper-utils/generated/index.gen.ts
+++ packages/@zipper-utils/generated/index.gen.ts
@@ -2,6 +2,5 @@
 
 export * from '../src/utils/crypto.utils';
 export { default as cryptoUtils } from '../src/utils/crypto.utils';
-export * from '../src/utils/get-last-run-version';
 export * from '../src/utils/safe-json';
 export * from '../src/utils/url';
