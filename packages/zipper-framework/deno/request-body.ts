efd0920143a1519dd96b9732764e12db9bca1fe6
imadha <ibu@zipper.works>
2023-04-05 11:28:28 -0500

    Build finally working

--- packages/zipper-framework/deno/request-body.ts
+++ packages/zipper-framework/deno/request-body.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore <ignore-file-extension-for-node-compatibility>
 import { AppInfo, OriginalRequest, Inputs, UserInfo } from './types.d.ts';
 
 export type RequestBody = {
