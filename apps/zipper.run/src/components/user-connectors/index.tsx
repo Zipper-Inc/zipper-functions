543973bf6c2e5c1bdb3f411c1c7db2369766b5b5
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-04-05 23:52:09 +0300

    added user connectors components

---
+++ apps/zipper.run/src/components/user-connectors/index.tsx
@@ -0,0 +1,39 @@
+import { VStack } from '@chakra-ui/react';
+import UserConnector from './user-connector';
+import {
+  ConnectorActionProps,
+  ConnectorType,
+  UserAuthConnector,
+} from '@zipper/types';
+
+type AuthUserConnectorsProps = {
+  userAuthConnectors: UserAuthConnector[];
+  actions: Record<ConnectorType, ConnectorActionProps>;
+};
+
+const AuthUserConnectors: React.FC<AuthUserConnectorsProps> = ({
+  actions,
+  userAuthConnectors,
+}) => {
+  return (
+    <VStack
+      as="ul"
+      alignItems="stretch"
+      maxW="container.sm"
+      minW={500}
+      bgColor="gray.50"
+      p={6}
+      rounded="2xl"
+      spacing="2.5"
+      listStyleType="none"
+    >
+      {userAuthConnectors.map((uac) => (
+        <li key={uac.type}>
+          <UserConnector connector={uac} {...actions[uac.type]} />
+        </li>
+      ))}
+    </VStack>
+  );
+};
+
+export default AuthUserConnectors;
