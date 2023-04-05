e574dcd62b8499f1e5d79be7e3d5244bae268720
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-04-05 23:42:41 +0300

    added connector related types

---
+++ packages/@zipper-types/src/types/connector.ts
@@ -0,0 +1,4 @@
+export type ConnectorActionProps = {
+  onDelete: VoidFunction;
+  authUrl: string;
+};
--- packages/@zipper-types/src/types/github-connector.ts
+++ packages/@zipper-types/src/types/github-connector.ts
@@ -4,13 +4,31 @@ export type GitHubAuthTokenResponse = {
   scope: string;
 };
 
-export type GitHubCheckTokenResponse = {
+export type GithubConnectorUserAuthMetadata = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+};
+
+export type GithubConnectorAuthMetadata = {
   id: number;
   url: string;
   scopes: string[] | null;
-  token: string;
-  token_last_eight: string;
-  hashed_token: string;
   app: {
     url: string;
     name: string;
@@ -22,24 +40,11 @@ export type GitHubCheckTokenResponse = {
   created_at: string;
   fingerprint: string;
   expires_at: string;
-  user: {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  };
+  user: GithubConnectorUserAuthMetadata;
+};
+
+export type GitHubCheckTokenResponse = GithubConnectorAuthMetadata & {
+  token: string;
+  token_last_eight: string;
+  hashed_token: string;
 };
