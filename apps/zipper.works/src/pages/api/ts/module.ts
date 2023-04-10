a434bbb4b15d450366dea64175a6779ac095a3bb
imadha <ibu@zipper.works>
2023-04-10 14:59:15 -0500

    Make redirects work, clean up code

--- apps/zipper.works/src/pages/api/ts/module.ts
+++ apps/zipper.works/src/pages/api/ts/module.ts
@@ -1,8 +1,70 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as eszip from '@deno/eszip';
 import { BuildCache, getModule } from '~/utils/eszip-build-cache';
+import { LoadResponseModule } from '@deno/eszip/types/loader';
 
 const buildCache = new BuildCache();
+
+function respondWithRawModule({
+  rootModule,
+  res,
+}: {
+  rootModule: LoadResponseModule;
+  res: NextApiResponse;
+}) {
+  if (rootModule.headers) {
+    Object.keys(rootModule.headers).forEach((k) =>
+      res.setHeader(k, rootModule.headers?.[k] || ''),
+    );
+  }
+
+  return res
+    .status(200)
+    .setHeader('Content-Type', 'text/typescript')
+    .send(rootModule.content);
+}
+
+async function respondWithBundle({
+  moduleUrl,
+  rootModule,
+  res,
+}: {
+  moduleUrl: string;
+  rootModule: LoadResponseModule;
+  res: NextApiResponse;
+}) {
+  const didRedirect = moduleUrl !== rootModule.specifier;
+  let handleRedirect: undefined | ((s: string) => string);
+
+  if (didRedirect) {
+    // get the end file, something like `mod.ts`
+    const filename = moduleUrl.substring(moduleUrl.lastIndexOf('/') + 1);
+    // remove the `mod.ts` from the original url
+    const originalPath = moduleUrl.replace(filename, '');
+    // get equivalent path on the redirected url
+    const redirectedPath = rootModule.specifier.replace(filename, '');
+
+    handleRedirect = (specifier: string) =>
+      specifier.replace(redirectedPath, originalPath);
+  }
+
+  const bundle: Record<string, string> = {};
+
+  await eszip.build([rootModule.specifier], async (specifier) => {
+    const bundlePath = handleRedirect ? handleRedirect(specifier) : specifier;
+
+    if (specifier === rootModule.specifier) {
+      bundle[bundlePath] = rootModule.content;
+      return rootModule;
+    } else {
+      const mod = await getModule(specifier, buildCache);
+      if (mod?.content) bundle[bundlePath] = mod.content;
+      return mod;
+    }
+  });
+
+  return res.status(200).send(JSON.stringify(bundle));
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,50 +81,11 @@ export default async function handler(
     ? req.query.x.join(',')
     : req.query.x;
 
-  const filename = moduleUrl.substring(moduleUrl.lastIndexOf('/') + 1);
-  const modulePath = moduleUrl.replace(filename, '');
-
   const rootModule = await getModule(moduleUrl, buildCache);
 
   if (!rootModule) return res.status(404).send('Module not found');
 
-  // If there is no bundle param, just return the module
-  if (!shouldBundle) {
-    if (rootModule.headers) {
-      Object.keys(rootModule.headers).forEach((k) =>
-        res.setHeader(k, rootModule.headers?.[k] || ''),
-      );
-    }
-
-    return res
-      .status(200)
-      .setHeader('Content-Type', 'text/typescript')
-      .send(rootModule.content);
-  }
-
-  const didRedirect = moduleUrl !== rootModule.specifier;
-  let handleRedirect: undefined | ((s: string) => string);
-  if (didRedirect) {
-    const filename = moduleUrl.substring(moduleUrl.lastIndexOf('/') + 1);
-    const modulePath = moduleUrl.replace(filename, '');
-    handleRedirect = (specifier: string) =>
-      specifier.replace(specifier.replace(filename, ''), modulePath);
-  }
-
-  const bundle: Record<string, string> = {};
-
-  await eszip.build([rootModule.specifier], async (specifierPassedIn) => {
-    const specifier = handleRedirect ? handleRedirect(specifierPassedIn) : specifierPassedIn;
-
-    if (specifier === rootModule.specifier) {
-      bundle[specifier] = rootModule.content;
-      return rootModule;
-    } else {
-      const mod = await getModule(specifier, buildCache);
-      if (mod?.content) bundle[specifier] = mod.content;
-      return mod;
-    }
-  });
-
-  return res.status(200).send(JSON.stringify(bundle));
+  return shouldBundle
+    ? respondWithBundle({ moduleUrl, rootModule, res })
+    : respondWithRawModule({ rootModule, res });
 }
