4b06944f95f81a274388b2c67ad7d5ca977c3c1c
Ariel Conti <arielconti10@gmail.com>
2023-04-13 00:11:16 +0100

    openai connector

--- apps/zipper.works/src/connectors/connectors.ts
+++ apps/zipper.works/src/connectors/connectors.ts
@@ -1,10 +1,12 @@
 import { Connector, ConnectorId } from './createConnector';
 import { githubConnector } from './github';
 import { slackConnector } from './slack';
+import { openaiConnector } from './openai';
 
 export const connectors: Record<ConnectorId, Connector> = {
   github: githubConnector,
   slack: slackConnector,
+  openai: openaiConnector,
 };
 
 export default connectors;
