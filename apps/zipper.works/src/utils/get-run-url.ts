cb8d5438f6c83503e28fb5328336edacc1d45e3b
Ibrahim Madha <imadha@users.noreply.github.com>
2023-04-14 10:31:45 -0500

    Revert this file (#119)

--- apps/zipper.works/src/utils/get-run-url.ts
+++ apps/zipper.works/src/utils/get-run-url.ts
@@ -1,17 +1,11 @@
-import { normalizeAppPath } from '@zipper/utils';
-import path from 'path';
-
 export default function getRunUrl(
   slug: string,
   version: string | null | undefined = 'latest',
   filename?: string,
 ) {
-  const runPath = path.join(
-    `run/${slug}/${version}`,
-    normalizeAppPath(filename),
-  );
+  const path = `run/${slug}/${version}/${filename || 'main.ts'}`;
 
   return `${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${
     process.env.NEXT_PUBLIC_HOST
-  }${process.env.NODE_ENV === 'production' ? '' : ':3000'}/${runPath}`;
+  }${process.env.NODE_ENV === 'production' ? '' : ':3000'}/${path}`;
 }
