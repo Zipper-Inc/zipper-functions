6123307091f44b35adeb9d4f4d3a53ea3c023c05
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-04-19 21:55:27 +0300

    added constants files and exported cookie name and tempUserId header key

--- packages/@zipper-utils/generated/index.gen.ts
+++ packages/@zipper-utils/generated/index.gen.ts
@@ -10,3 +10,4 @@ export * from '../src/utils/get-app-link';
 export * from '../src/utils/object';
 export * from '../src/utils/safe-json';
 export * from '../src/utils/url';
+export * from '../src/constants';
