8ce50c6535541b8d3766dc74ec86375f80950641
Wynn Netherland <wynn.netherland@gmail.com>
2023-04-22 16:01:14 -0500

    Use typescript

--- apps/zipper.run/src/pages/api/png/[runId].ts
+++ apps/zipper.run/src/pages/api/png/[runId].ts
@@ -1,19 +1,23 @@
 import { chromium } from 'playwright';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const host = req.headers['x-forwarded-host'] || req.headers.host;
   const proto = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-  const path = req.url.replace('api/png', 'run');
+  const path = req.url?.replace('api/png', 'run');
 
   const runUrl = `${proto}://${host}${path}`;
 
   const selector = '#smart-function-output';
   const browser = await chromium.launch();
-  const page = await browser.newPage();
+  const context = await browser.newContext({ deviceScaleFactor: 2 });
+  const page = await context.newPage();
   await page.setViewportSize({
     width: 1920,
     height: 1280,
-    deviceScaleFactor: 2,
   });
   await page.goto(runUrl);
   const output = page.locator(selector);
