af37068f7ecd0aa39c02b31493c6cdbb7cdca3bc
Sachin Ranchod <sachin@zipper.works>
2023-04-24 14:17:46 -0700

    Add a function output context and a refresh action (#136)
    * add a context for function output
    
    * add missing file
    
    * passh path and inputs around
    
    * refresh action
    
    * minor refractor
    
    * remove console.log
    
    * forgot to apply changes to the run app component
    
    * don't export ActionBase

--- packages/@zipper-ui/src/components/function-output/action-button.tsx
+++ packages/@zipper-ui/src/components/function-output/action-button.tsx
@@ -1,21 +1,29 @@
 import { Button, Flex, Spinner } from '@chakra-ui/react';
-import { useState } from 'react';
-import { FunctionOutputProps } from './types';
+import { useContext, useState } from 'react';
 import { normalizeAppPath } from '@zipper/utils';
+import { FunctionOutputContext } from './function-output-context';
 
-export function ActionButton({
-  action,
-  setExpandedResult,
-  setModalResult,
-  setOverallResult,
-  getRunUrl,
-}: Omit<FunctionOutputProps, 'result'> & { action: Zipper.Action }) {
+export function ActionButton({ action }: { action: Zipper.Action }) {
+  const {
+    setModalResult,
+    setExpandedResult,
+    setOverallResult,
+    getRunUrl,
+    path,
+    inputs,
+  } = useContext(FunctionOutputContext);
+
   async function runScript() {
-    const res = await fetch(getRunUrl(action.path), {
-      method: 'POST',
-      body: JSON.stringify(action.inputs || []),
-      credentials: 'include',
-    });
+    const res = await fetch(
+      getRunUrl(action.showAs === 'refresh' ? path : action.path),
+      {
+        method: 'POST',
+        body: JSON.stringify(
+          action.showAs === 'refresh' ? inputs || [] : action.inputs || [],
+        ),
+        credentials: 'include',
+      },
+    );
     const text = await res.text();
 
     switch (action.showAs) {
@@ -26,6 +34,9 @@ export function ActionButton({
         setExpandedResult(text);
         break;
       case 'replace_all':
+        setOverallResult(text);
+        break;
+      case 'refresh':
         setOverallResult(text);
         break;
       default:
--- packages/@zipper-ui/src/components/function-output/action-component.tsx
+++ packages/@zipper-ui/src/components/function-output/action-component.tsx
@@ -1,23 +1,8 @@
 import { ActionButton } from './action-button';
-import { FunctionOutputProps } from './types';
 
-export function ActionComponent({
-  action,
-  setExpandedResult,
-  setModalResult,
-  setOverallResult,
-  getRunUrl,
-}: Omit<FunctionOutputProps, 'result'> & { action: Zipper.Action }) {
+export function ActionComponent({ action }: { action: Zipper.Action }) {
   switch (action.actionType) {
     default:
-      return (
-        <ActionButton
-          action={action}
-          setExpandedResult={setExpandedResult}
-          setModalResult={setModalResult}
-          setOverallResult={setOverallResult}
-          getRunUrl={getRunUrl}
-        />
-      );
+      return <ActionButton action={action} />;
   }
 }
