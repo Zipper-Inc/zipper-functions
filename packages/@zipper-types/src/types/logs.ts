20569f8b938eedf32ca1c728764c6a868c5fcfa8
imadha <ibu@zipper.works>
2023-04-26 17:28:34 -0500

    Update types

--- packages/@zipper-types/src/types/logs.ts
+++ packages/@zipper-types/src/types/logs.ts
@@ -1,22 +1,25 @@
-export type LogMethod =
-  | 'log'
-  | 'debug'
-  | 'info'
-  | 'warn'
-  | 'error'
-  | 'table'
-  | 'clear'
-  | 'time'
-  | 'timeEnd'
-  | 'count'
-  | 'assert';
+export enum LogMethod {
+  log = 'log',
+  debug = 'debug',
+  info = 'info',
+  warn = 'warn',
+  error = 'error',
+  table = 'table',
+  clear = 'clear',
+  time = 'time',
+  timeEnd = 'timeEnd',
+  count = 'count',
+  assert = 'assert',
+}
+
+export const logMethods = Object.values(LogMethod);
 
 export interface Log {
   id: string;
   // The log method
   method: LogMethod;
   // The arguments passed to console API
-  data: any[];
+  data: Zipper.Serializable[];
   // Time of log
   timestamp?: string;
 }
@@ -24,3 +27,8 @@ export interface Log {
 export type LogRecord = Omit<Log, 'timestamp'> & {
   timestamp: number;
 };
+
+export type ConsoleLogger = {
+  fetch: (fromTimestamp?: number) => Promise<Log[]>;
+  send: (log: LogRecord) => void;
+} & Record<LogMethod, (...data: Log['data']) => void>;
