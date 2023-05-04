88af9edfa92836674c9c6bfeda63779de88f0a79
Sachin Ranchod <sachin@zipper.works>
2023-05-04 13:59:20 -0500

    get sentry added to both apps

--- apps/zipper.run/next.config.js
+++ apps/zipper.run/next.config.js
@@ -1,3 +1,7 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -6,8 +10,40 @@
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
  */
+
 function getConfig(config) {
-  return config;
+  return withSentryConfig(
+    config,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+
+      // Suppresses source map uploading logs during build
+      silent: true,
+
+      org: 'zipper-01',
+      project: 'zipper-dot-run',
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: '/monitoring',
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+    },
+  );
 }
 
 module.exports = getConfig({
@@ -23,11 +59,9 @@ module.exports = getConfig({
     return [
       {
         source: '/removeConnector/:appId/:connectorType/:path*',
-        destination: `${
-          process.env.NODE_ENV === 'production' ? 'https' : 'http'
-        }://${
-          process.env.NEXT_PUBLIC_ZIPPER_HOST
-        }/api/app/:appId/removeConnector/:connectorType`,
+        destination: `${process.env.NODE_ENV === 'production' ? 'https' : 'http'
+          }://${process.env.NEXT_PUBLIC_ZIPPER_HOST
+          }/api/app/:appId/removeConnector/:connectorType`,
       },
     ];
   },
