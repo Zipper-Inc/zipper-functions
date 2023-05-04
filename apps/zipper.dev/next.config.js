850dccacc0fa161915331e00649715d9350705b2
Sachin Ranchod <sachin@zipper.works>
2023-05-04 14:30:47 -0500

    Get sentry added to both apps (#155)
    * get sentry added to both apps
    
    * fix build errors
    
    * remove unused util

--- apps/zipper.dev/next.config.js
+++ apps/zipper.dev/next.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { env } = require('./src/server/env');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
  * Don't be scared of the generics here.
@@ -11,7 +12,38 @@ const { env } = require('./src/server/env');
  * @constraint {{import('next').NextConfig}}
  */
 function getConfig(config) {
-  return config;
+  return withSentryConfig(
+    config,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+
+      // Suppresses source map uploading logs during build
+      silent: true,
+
+      org: 'zipper-01',
+      project: 'zipper-dot-dev',
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: '/monitoring',
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+    },
+  );
 }
 
 /**
