1d97cf19a7d56a3194b5093f50c42ad3953ee4dd
Ariel Conti <arielconti10@gmail.com>
2023-05-05 14:43:34 +0100

    action dropdown

--- packages/@zipper-ui/src/components/function-output/action-component.tsx
+++ packages/@zipper-ui/src/components/function-output/action-component.tsx
@@ -1,7 +1,12 @@
 import { ActionButton } from './action-button';
+import { ActionDropdown } from './action-dropdown';
 
 export function ActionComponent({ action }: { action: Zipper.Action }) {
   switch (action.actionType) {
+    case 'dropdown':
+      return <ActionDropdown action={action} />;
+    case 'button':
+      return <ActionButton action={action} />;
     default:
       return <ActionButton action={action} />;
   }
