e1fdbe325fab508377f85f5d43760f235034a6e6
Ariel Conti <arielconti10@gmail.com>
2023-05-15 15:57:38 +0100

    updating clerk on zipper dev

--- apps/zipper.dev/src/middleware.ts
+++ apps/zipper.dev/src/middleware.ts
@@ -1,19 +1,21 @@
-import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { ZIPPER_TEMP_USER_ID_COOKIE_NAME } from '@zipper/utils';
 
-export default withClerkMiddleware((req: NextRequest) => {
-  const res = NextResponse.next();
-  const { userId } = getAuth(req);
-  if (!userId && !req.cookies.get(ZIPPER_TEMP_USER_ID_COOKIE_NAME)) {
-    res.cookies.set(
-      ZIPPER_TEMP_USER_ID_COOKIE_NAME,
-      `temp__${crypto.randomUUID()}`,
-    );
-  }
+export default authMiddleware({
+  afterAuth: async ({ userId }, request: NextRequest) => {
+    const res = NextResponse.next();
 
-  return res;
+    if (!userId && !request.cookies.get(ZIPPER_TEMP_USER_ID_COOKIE_NAME)) {
+      res.cookies.set(
+        ZIPPER_TEMP_USER_ID_COOKIE_NAME,
+        `temp__${crypto.randomUUID()}`,
+      );
+    }
+
+    return res;
+  },
 });
 
 // Stop Middleware running on static files
