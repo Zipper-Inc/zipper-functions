918a5c558f4ff85a09917d195e621b3a3e3ad678
Sachin Ranchod <sachin@zipper.works>
2023-06-05 15:47:58 -0700

    next.config was using old env vars

--- apps/zipper.run/next.config.js
+++ apps/zipper.run/next.config.js
@@ -60,17 +60,12 @@ module.exports = getConfig({
     return [
       {
         source: '/_zipper/removeConnector/:appId/:connectorType/:path*',
-        destination: `${
-          process.env.NODE_ENV === 'production' ? 'https' : 'http'
-        }://${
-          process.env.NEXT_PUBLIC_ZIPPER_HOST
-        }/api/app/:appId/removeConnector/:connectorType`,
+        destination: `${process.env.NEXT_PUBLIC_ZIPPER_DOT_DEV_URL
+          }/api/app/:appId/removeConnector/:connectorType`,
       },
       {
         source: '/_zipper/app/info/:appSlug/:path*',
-        destination: `${
-          process.env.NODE_ENV === 'production' ? 'https' : 'http'
-        }://${process.env.NEXT_PUBLIC_ZIPPER_HOST}/api/app/info/:appSlug`,
+        destination: `${process.env.NEXT_PUBLIC_ZIPPER_DOT_DEV_URL}/api/app/info/:appSlug`,
       },
       {
         source: '/run/history/:runId.png',
