e43d1d68fd068bc7fef7b97bd4688b86b00705bf
imadha <ibu@zipper.works>
2023-06-09 10:33:40 -0500

    Fix types

--- apps/zipper.dev/src/types.d.ts
+++ apps/zipper.dev/src/types.d.ts
@@ -1,6 +1,21 @@
-import type { DefaultUser } from 'next-auth';
+declare module 'monaco-jsx-highlighter' {
+  import MonacoJsxHighlighter from 'monaco-jsx-highlighter';
+
+  export default class MonacoJsxHighlighter {
+    constructor(
+      monaco: any,
+      parse: any,
+      traverse: any,
+      editor: any,
+    ): MonacoJsxHighlighter;
+
+    highlightOnDidChangeModelContent(debounceMs: number): void;
+    addJSXCommentCommand(): void;
+  }
+}
 
 declare module 'next-auth' {
+  import type { DefaultUser } from 'next-auth';
   interface Session {
     user?: DefaultUser & {
       id: string;
