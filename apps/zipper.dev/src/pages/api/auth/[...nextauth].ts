c2f60d3d589bc088d72f2c5e9f1d681cc4ba902d
Sachin Ranchod <sachin@zipper.works>
2023-06-16 16:16:37 -0700

    include org membership in getSession output

--- apps/zipper.dev/src/pages/api/auth/[...nextauth].ts
+++ apps/zipper.dev/src/pages/api/auth/[...nextauth].ts
@@ -66,6 +66,42 @@ export function PrismaAdapter(p: PrismaClient): Adapter {
   };
 }
 
+async function refreshAccessToken(token: TokenSet) {
+  try {
+    // https://accounts.google.com/.well-known/openid-configuration
+    // We need the `token_endpoint`.
+    if (!token.refresh_token) throw new Error('No refresh token');
+
+    const response = await fetch('https://oauth2.googleapis.com/token', {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        client_id: process.env.NEXTAUTH_GOOGLE_CLIENT_ID!,
+        client_secret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET!,
+        grant_type: 'refresh_token',
+        refresh_token: token.refresh_token,
+      }),
+      method: 'POST',
+    });
+
+    const tokens = await response.json();
+
+    if (!response.ok) throw tokens;
+
+    return {
+      ...token, // Keep the previous token properties
+      access_token: tokens.access_token,
+      // expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+      // Fall back to old refresh token, but note that
+      // many providers may only allow using a refresh token once.
+      refresh_token: tokens.refresh_token ?? token.refresh_token,
+    };
+  } catch (error) {
+    console.error('Error refreshing access token', error);
+    // The error property will be used client-side to handle the refresh token error
+    return { ...token, error: 'RefreshAccessTokenError' as const };
+  }
+}
+
 export const authOptions = {
   // Configure one or more authentication providers
   session: {
@@ -91,62 +127,51 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
-    async jwt({ token, account, trigger, session }) {
-      if (trigger === 'update' && session.organizations) {
-        // validate and update the list of organizations
-      }
-
-      if (!account?.refresh_token) return token;
-
+    async jwt({ token: _token, user, account, trigger, session }) {
+      const token = { ..._token };
+      // Initial sign in (only time account is not null)
       if (account) {
         // Save the access token and refresh token in the JWT on the initial login
         return {
+          ...token,
           access_token: account.access_token,
           expires_at: account.expires_at,
           refresh_token: account.refresh_token,
+          organizationMemberships: await prisma.organizationMembership.findMany(
+            {
+              where: { userId: user.id },
+            },
+          ),
         };
-      } else if (Date.now() < token.expires_at * 1000) {
+      }
+
+      if (trigger === 'update' && session) {
+        console.log('update session', session);
+        // validate and update the list of organizations
+      }
+
+      if (!token.expires_at) return token;
+
+      // Not initial sign in
+      // token is still good; carry on
+      if (Date.now() < token.expires_at * 1000) {
         // If the access token has not expired yet, return it
         return token;
-      } else {
-        // If the access token has expired, try to refresh it
-        try {
-          // https://accounts.google.com/.well-known/openid-configuration
-          // We need the `token_endpoint`.
-          if (!token.refresh_token) throw new Error('No refresh token');
-
-          const response = await fetch('https://oauth2.googleapis.com/token', {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({
-              client_id: process.env.NEXTAUTH_GOOGLE_CLIENT_ID!,
-              client_secret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET!,
-              grant_type: 'refresh_token',
-              refresh_token: token.refresh_token,
-            }),
-            method: 'POST',
-          });
-
-          const tokens = await response.json();
-
-          if (!response.ok) throw tokens;
-
-          return {
-            ...token, // Keep the previous token properties
-            access_token: tokens.access_token,
-            // expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
-            // Fall back to old refresh token, but note that
-            // many providers may only allow using a refresh token once.
-            refresh_token: tokens.refresh_token ?? token.refresh_token,
-          };
-        } catch (error) {
-          console.error('Error refreshing access token', error);
-          // The error property will be used client-side to handle the refresh token error
-          return { ...token, error: 'RefreshAccessTokenError' as const };
-        }
       }
+
+      // If the access token has expired, try to refresh it
+      return refreshAccessToken(token);
     },
     async session({ session, token }) {
       session.error = token.error;
+      session.user = {
+        name: token.name,
+        email: token.email,
+        image: token.picture,
+      };
+      session.currentOrganizationId = token.currentOrganizationId;
+      session.organizationMemberships = token.organizationMemberships;
+
       return session;
     },
   },
@@ -157,6 +182,8 @@ export default NextAuth(authOptions);
 declare module 'next-auth' {
   interface Session {
     error?: 'RefreshAccessTokenError';
+    organizationMemberships: { organizationId: string; role: string }[];
+    currentOrganizationId?: string;
   }
 }
 
@@ -164,8 +191,9 @@ declare module 'next-auth/jwt' {
   interface JWT {
     access_token: string;
     expires_at: number;
-    refresh_token: string;
+    refresh_token?: string;
     error?: 'RefreshAccessTokenError';
     organizationMemberships: { organizationId: string; role: string }[];
+    currentOrganizationId?: string;
   }
 }
