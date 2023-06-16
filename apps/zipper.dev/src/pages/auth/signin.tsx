74143a6f041e0f0e3c14fd6043e81688c5021151
Sachin Ranchod <sachin@zipper.works>
2023-06-16 08:32:29 -0700

    get nextauth installed; set up prisma tables

---
+++ apps/zipper.dev/src/pages/auth/signin.tsx
@@ -0,0 +1,52 @@
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next';
+import { getProviders, signIn } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../api/auth/[...nextauth]';
+import { getCsrfToken } from 'next-auth/react';
+
+export default function SignIn({
+  providers,
+  csrfToken,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      {/* <form method="post" action="/api/auth/signin/email">
+        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <label>
+          Email address
+          <input type="email" id="email" name="email" />
+        </label>
+        <button type="submit">Sign in with Email</button>
+      </form> */}
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  const csrfToken = await getCsrfToken(context);
+
+  // If the user is already logged in, redirect.
+  // Note: Make sure not to redirect to the same page
+  // To avoid an infinite loop!
+  if (session) {
+    return { redirect: { destination: '/' } };
+  }
+
+  const providers = await getProviders();
+
+  return {
+    props: { providers: providers ?? [], csrfToken },
+  };
+}
