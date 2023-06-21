4f679d33bd072604bce4f9f1d5b5b01fc0cd5c9b
imadha <ibu@zipper.works>
2023-06-20 21:34:32 -0500

    Now memberships work

--- apps/zipper.dev/src/pages/api/omni/organizations/[orgId]/memberships/index.ts
+++ apps/zipper.dev/src/pages/api/omni/organizations/[orgId]/memberships/index.ts
@@ -1,22 +1,106 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '~/server/prisma';
+import {
+  successResponse,
+  errorResponse,
+  methodNotAllowed,
+  createOmniApiHandler,
+  OmniApiError,
+  simpleErrorResponse,
+} from '~/server/utils/omni.utils';
+import { HttpMethod as Method, HttpStatusCode as Status } from '@zipper/types';
 
-export default async function omniOrganizatioMemberAdd(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  // check auth
+const DEFAULT_ROLE = 'member';
 
-  // get inputs
-  // console.log(await req.body.json());
+export default createOmniApiHandler(async (req, res) => {
+  const organizationId = req.query.orgId as string;
+  if (!organizationId) {
+    return simpleErrorResponse({
+      res,
+      message: 'Missing org id',
+      status: Status.BAD_REQUEST,
+    });
+  }
 
-  // create prisma
+  switch (req.method) {
+    // CREATE
+    case Method.PATCH:
+    case Method.POST:
+    case Method.PUT: {
+      const errors: OmniApiError[] = [];
 
-  // return the created user object
+      const membershipsToCreate: {
+        userId: string;
+        role: string;
+      }[] = req.body.memberships;
 
-  return res.status(501).end(
-    JSON.stringify({
-      ok: false,
-      called: 'omni/organization/[id]/addMember not yet implemented',
-    }),
-  );
-}
+      const noMemberships =
+        !Array.isArray(membershipsToCreate) || !membershipsToCreate.length;
+      if (noMemberships) {
+        errors.push({ message: 'Missing or empty array of memberships' });
+      }
+
+      if (
+        noMemberships ||
+        membershipsToCreate.find((membership) => !membership.userId)
+      ) {
+        errors.push({ message: 'Each membership must have a userId' });
+      }
+
+      if (errors.length) {
+        return errorResponse({
+          res,
+          body: {
+            ok: false,
+            errors,
+          },
+          status: Status.BAD_REQUEST,
+        });
+      }
+
+      const createdMemberships = await Promise.all(
+        membershipsToCreate.map(({ userId, role }) =>
+          prisma.organizationMembership.create({
+            data: {
+              userId,
+              organizationId,
+              role: role || DEFAULT_ROLE,
+            },
+          }),
+        ),
+      );
+
+      return successResponse({
+        res,
+        body: {
+          ok: true,
+          data: {
+            created: true,
+            memberships: createdMemberships,
+          },
+        },
+      });
+    }
+
+    // READ
+    case Method.GET: {
+      const organization = await prisma.organization.findFirst({
+        where: { id: organizationId },
+      });
+      const memberships = await prisma.organizationMembership.findMany({
+        where: { organizationId },
+        include: { user: true },
+      });
+
+      return successResponse({
+        res,
+        body: {
+          ok: true,
+          data: { organization, memberships },
+        },
+      });
+    }
+
+    default:
+      return methodNotAllowed({ method: req.method, res });
+  }
+});
