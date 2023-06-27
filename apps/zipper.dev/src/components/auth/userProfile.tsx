f89682f42c7bba486f3fe51d44767d9af125460e
Ariel Conti <arielconti10@gmail.com>
2023-06-27 22:20:46 +0100

    updating user slug

--- apps/zipper.dev/src/components/auth/userProfile.tsx
+++ apps/zipper.dev/src/components/auth/userProfile.tsx
@@ -1,8 +1,25 @@
 import { useUser } from '~/hooks/use-user';
-import { Text, Box, Stack, Avatar, Divider } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import {
+  Text,
+  Box,
+  Stack,
+  Avatar,
+  Divider,
+  HStack,
+  Input,
+  Button,
+  InputRightElement,
+  Icon,
+  InputGroup,
+} from '@chakra-ui/react';
 import { SiGithub } from 'react-icons/si';
 import { FcGoogle } from 'react-icons/fc';
 import { trpc } from '~/utils/trpc';
+import { HiPencil } from 'react-icons/hi';
+import { MIN_SLUG_LENGTH, useAppSlug } from '~/hooks/use-app-slug';
+import { HiExclamationTriangle } from 'react-icons/hi2';
+import { CheckIcon } from '@chakra-ui/icons';
 
 export default function UserProfile() {
   const { user, isLoaded } = useUser();
@@ -10,6 +27,34 @@ export default function UserProfile() {
     enabled: isLoaded,
   });
 
+  const [isEditingUsername, setIsEditingUsername] = useState(false);
+  const [username, setUsername] = useState('');
+  useEffect(() => {
+    if (isLoaded && user?.username) {
+      setUsername(user.username);
+    }
+  }, [user]);
+
+  const { slugExists: _slugExists, appSlugQuery } = useAppSlug(username);
+  const isSlugValid =
+    appSlugQuery.isFetched && username && username.length >= MIN_SLUG_LENGTH;
+
+  const slugExists = username === user?.username ? false : _slugExists;
+  const disableSave = slugExists || username === user?.username || username.length < MIN_SLUG_LENGTH;
+
+  const updateUsername = trpc.useMutation('user.updateUserSlug', {
+    
+    async onSuccess() {
+      // need to refresh user
+
+    },
+  });
+  const handleSaveUsername = async () => {
+    if (username.length < MIN_SLUG_LENGTH) {
+      return;
+    }
+    await updateUsername.mutateAsync({ username })
+  }
   return (
     <>
       {user && (
@@ -40,30 +85,87 @@ export default function UserProfile() {
             </Text>
             <Text fontSize="sm">{user.email}</Text>
           </Stack>
+          <Stack>
+            <Text fontSize="md" fontWeight="bold">
+              Username
+            </Text>
+            <Stack>
+              {!isEditingUsername ? (
+                <HStack
+                  _hover={{ cursor: 'pointer' }}
+                  onClick={() => {
+                    setIsEditingUsername(true);
+                  }}
+                >
+                  <Text fontSize="sm">{user.username}</Text>
+                  <HiPencil />
+                </HStack>
+              ) : (
+                <HStack maxW={200}>
+                  <InputGroup>
+                    <Input
+                      size="sm"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
+                    />
+
+                    {isSlugValid && !updateUsername.isLoading && (
+                      <InputRightElement
+                        children={
+                          slugExists ? (
+                            <Icon as={HiExclamationTriangle} color="red.500" />
+                          ) : (
+                            <CheckIcon color="green.500" />
+                          )
+                        }
+                      />
+                    )}
+                    
+                  </InputGroup>
+                  <Button
+                    size="sm"
+                    variant="link"
+                    colorScheme="purple"
+                    isDisabled={disableSave}
+                    onClick={() => {
+                      handleSaveUsername();
+                      setIsEditingUsername(false);
+                    }}
+                  >
+                    Save
+                  </Button>
+                </HStack>
+              )}
+            </Stack>
+          </Stack>
         </Box>
       )}
       <Text fontSize={'xl'} mt="8">
         Connected Accounts
       </Text>
       <Divider mb="4" mt={2} />
-      {accounts && accounts.map((account) => (
-        <Box key={account.provider} mt="4">
-          <Text fontSize="sm" fontWeight="bold">
-            {account.provider === 'github' && (
-              <Stack direction="row" alignItems="center" gap={2}>
-                <SiGithub size={24} color="#181717" />
-                <Text>GitHub</Text>
-              </Stack>
-            )}
-            {account.provider === 'google' && (
-              <Stack direction="row" alignItems="center" gap={2}>
-                <FcGoogle size={24} />
-                <Text>Google</Text>
-              </Stack>
-            )}
-          </Text>
-        </Box>
-      ))}
+      {accounts &&
+        accounts.map((account) => (
+          <Box key={account.provider} mt="4">
+            <Text fontSize="sm" fontWeight="bold">
+              {account.provider === 'github' && (
+                <Stack direction="row" alignItems="center" gap={2}>
+                  <SiGithub size={24} color="#181717" />
+                  <Text>GitHub</Text>
+                </Stack>
+              )}
+              {account.provider === 'google' && (
+                <Stack direction="row" alignItems="center" gap={2}>
+                  <FcGoogle size={24} />
+                  <Text>Google</Text>
+                </Stack>
+              )}
+            </Text>
+          </Box>
+        ))}
     </>
   );
 }
