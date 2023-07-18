8faeca634088030c10ef57a9ce65e9a203da93fd
imadha <ibu@zipper.works>
2023-07-18 14:50:03 -0500

    Basic guts of a blog

---
+++ apps/blog/next.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './src/theme.config.tsx',
+});
+
+module.exports = withNextra();
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
---
+++ apps/blog/src/theme.config.tsx
@@ -0,0 +1,23 @@
+const config = {
+  footer: <p>MIT 2023 © Nextra.</p>,
+  head: ({ _title, meta }: any) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: false,
+  navs: [
+    {
+      url: 'https://github.com/shuding/nextra',
+      name: 'Nextra',
+    },
+  ],
+};
+
+export default config;
