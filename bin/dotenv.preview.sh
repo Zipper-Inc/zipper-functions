44aed4d9ad91c70abea2987e509ab68f18b57052
imadha <ibu@zipper.works>
2023-07-26 10:25:01 -0500

    Oops, missing quote

--- bin/dotenv.preview.sh
+++ bin/dotenv.preview.sh
@@ -1,6 +1,6 @@
 echo \
 "-v NEXT_PUBLIC_ZIPPER_DOT_DEV_HOST=$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST).localdev.me:9999" \
-"-v NEXT_PUBLIC_ZIPPER_DOT_RUN_HOST=$(dotenv -p RENDER_ZIPPER_DOT_RUN_HOST).localdev.me:9999 \
+"-v NEXT_PUBLIC_ZIPPER_DOT_RUN_HOST=$(dotenv -p RENDER_ZIPPER_DOT_RUN_HOST).localdev.me:9999" \
 "-v PUBLICLY_ACCESSIBLE_RPC_HOST=$(dotenv -p RENDER_DOT_DEV_HOST).onrender.com" \
 "-v NEXTAUTH_URL=https://$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST).localdev.me:9999" \
 "-v NODE_ENV=production"
