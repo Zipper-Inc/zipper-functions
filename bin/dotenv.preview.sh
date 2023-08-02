6c35277120df08f3003a12521959226d799a886c
imadha <ibu@zipper.works>
2023-08-02 08:24:31 -0500

    Make future URLs

--- bin/dotenv.preview.sh
+++ bin/dotenv.preview.sh
@@ -1,6 +1,6 @@
 echo \
-"-v NEXT_PUBLIC_ZIPPER_DOT_DEV_HOST=$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST).localdev.me:9999" \
-"-v NEXT_PUBLIC_ZIPPER_DOT_RUN_HOST=$(dotenv -p RENDER_ZIPPER_DOT_RUN_HOST).localdev.me:9999" \
+"-v NEXT_PUBLIC_ZIPPER_DOT_DEV_HOST=$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST| cut -b 20-).zpr.dev" \
+"-v NEXT_PUBLIC_ZIPPER_DOT_RUN_HOST=$(dotenv -p RENDER_ZIPPER_DOT_RUN_HOST| cut -b 20-).zpr.run" \
 "-v PUBLICLY_ACCESSIBLE_RPC_HOST=$(dotenv -p RENDER_DOT_DEV_HOST).onrender.com" \
-"-v NEXTAUTH_URL=https://$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST).localdev.me:9999" \
+"-v NEXTAUTH_URL=https://$(dotenv -p RENDER_ZIPPER_DOT_DEV_HOST| cut -b 20-).zpr.dev" \
 "-v NODE_ENV=production"
