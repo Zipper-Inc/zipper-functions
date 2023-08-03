5fad69af16033b382d49d85c93ded740b95488fe
Matt Verheaghe <mattverheaghe@Matts-MacBook-Pro.local>
2023-08-02 20:20:04 -0700

    Added support for formdata

--- packages/@zipper-utils/src/utils/parseBody.ts
+++ packages/@zipper-utils/src/utils/parseBody.ts
@@ -1,8 +1,17 @@
 export async function parseBody(
   request: Request,
 ): Promise<Record<string, any>> {
-  return request.headers.get('Content-Type') ===
-    'application/x-www-form-urlencoded'
-    ? Object.fromEntries(new URLSearchParams(await request.text()).entries())
-    : JSON.parse((await request.text()) || '{}');
+  const header = request.headers.get('Content-Type');
+  switch (true) {
+    case header === 'application/x-www-form-urlencoded':
+      return Object.fromEntries(
+        new URLSearchParams(await request.text()).entries(),
+      );
+    case header === 'application/json':
+      return JSON.parse((await request.text()) || '{}');
+    case header?.startsWith('multipart/form-data'):
+      return Object.fromEntries(await request.formData());
+    default:
+      return {};
+  }
 }
