468091f97e376ae6d1cb9c6b03e1511acb5ce835
Sachin Ranchod <sachin@zipper.works>
2023-08-07 21:28:40 -0700

    add chakra; add index content; rearrange a few things

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,14 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  reactStrictMode: true,
+  transpilePackages: [
+    '@zipper/framework',
+    '@zipper/types',
+    '@zipper/ui',
+    '@zipper/utils',
+  ],
+});
