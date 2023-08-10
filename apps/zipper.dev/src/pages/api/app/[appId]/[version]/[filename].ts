8f9ef12101c97b62586c1b65646d502f804bd0e7
Sachin Ranchod <sachin@zipper.works>
2023-08-09 20:57:37 -0700

    add ability to get src from eszip file

--- apps/zipper.dev/src/pages/api/app/[appId]/[version]/[filename].ts
+++ apps/zipper.dev/src/pages/api/app/[appId]/[version]/[filename].ts
@@ -1,5 +1,9 @@
+import { GetObjectCommand } from '@aws-sdk/client-s3';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '~/server/prisma';
+import s3Client from '~/server/s3';
+import { getAppVersionFromHash } from '~/utils/hashing';
+import { Parser } from '@deno/eszip';
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +12,9 @@ export default async function handler(
   const { query } = req;
 
   const appId = Array.isArray(query.appId) ? query.appId[0] : query.appId;
+  const version = Array.isArray(query.version)
+    ? query.version[0]
+    : query.version;
   const filename = Array.isArray(query.filename)
     ? query.filename[0]
     : query.filename;
@@ -21,7 +28,7 @@ export default async function handler(
     throw new Error("No filename so I can't look up the source code for you");
 
   const app = await prisma.app.findFirst({
-    where: { id: appId },
+    where: { id: appId, isPrivate: false },
     include: { scripts: true },
   });
 
@@ -30,18 +37,51 @@ export default async function handler(
   const loweredQueryFilename = filename.toLowerCase();
   console.log(loweredQueryFilename);
 
-  const script = app.scripts.find((script) => {
-    const loweredScriptFilename = script.filename.toLowerCase();
-    return (
-      loweredScriptFilename === loweredQueryFilename ||
-      loweredScriptFilename === `${loweredQueryFilename}.ts`
+  if (
+    version === 'latest' ||
+    version === getAppVersionFromHash(app.playgroundVersionHash)
+  ) {
+    const script = app.scripts.find((script) => {
+      const loweredScriptFilename = script.filename.toLowerCase();
+      return (
+        loweredScriptFilename === loweredQueryFilename ||
+        loweredScriptFilename === `${loweredQueryFilename}.ts`
+      );
+    });
+
+    if (!script)
+      throw new Error(
+        'No script found. Maybe your gave me the wrong filename?',
+      );
+
+    res.setHeader('Content-Type', 'text/typescript');
+
+    res.send(script.code);
+  } else {
+    let eszip: Buffer | undefined;
+    const foundFile = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: process.env.CLOUDFLARE_BUILD_FILE_BUCKET_NAME,
+        Key: `${app.id}/${version}`,
+      }),
     );
-  });
+    const byteArray = await foundFile.Body?.transformToByteArray();
 
-  if (!script)
-    throw new Error('No script found. Maybe your gave me the wrong filename?');
+    if (byteArray) {
+      console.log('found file on cloudflare');
+      eszip = Buffer.from(byteArray);
+    }
 
-  res.setHeader('Content-Type', 'text/typescript');
-
-  res.send(script.code);
+    if (eszip) {
+      Parser.createInstance().then(async (parser) => {
+        const specifiers = await parser.parseBytes(eszip!);
+        await parser.load();
+        const specifier = specifiers.find((specifier: string) =>
+          specifier.endsWith(`applet/src/${filename}.tsx`),
+        );
+        res.send(await parser.getModuleSource(specifier));
+        res.setHeader('Content-Type', 'text/typescript');
+      });
+    }
+  }
 }
