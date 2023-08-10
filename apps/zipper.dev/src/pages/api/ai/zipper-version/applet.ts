4cde1dbc5fbebd61c843dbc6c10ed5d6e886ebea
Ariel Conti <arielconti10@gmail.com>
2023-08-10 15:51:50 +0100

    feat: update models to gpt-4

--- apps/zipper.dev/src/pages/api/ai/zipper-version/applet.ts
+++ apps/zipper.dev/src/pages/api/ai/zipper-version/applet.ts
@@ -584,7 +584,7 @@ declare function Dropdown<I = Zipper.Inputs>(
   const openai = new OpenAIApi(conf);
   try {
     const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-16k-0613',
+      model: 'gpt-4-0613',
       messages: [
         {
           role: 'system',
