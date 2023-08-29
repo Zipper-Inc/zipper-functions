ba0867aa3ba394e7c5fc804f45e7bf33b961ae70
Ibrahim Madha <imadha@users.noreply.github.com>
2023-08-29 13:27:43 -0500

    Ibu - additional imports work round 1 (#364)
    * Do fancy JSX stuff in Eszip builder
    
    * Stop bundle from breaking
    
    * Move APIs over so it makes a little more sense
    
    * Try a different method for generating types
    
    * Oops

--- apps/zipper.dev/src/components/context/editor-context.tsx
+++ apps/zipper.dev/src/components/context/editor-context.tsx
@@ -28,6 +28,7 @@ import { uuid } from '@zipper/utils';
 import { prettyLog } from '~/utils/pretty-log';
 import { AppQueryOutput } from '~/types/trpc';
 import { getAppVersionFromHash } from '~/utils/hashing';
+import { isZipperImportUrl } from '~/utils/eszip-utils';
 import Fuse from 'fuse.js';
 
 /** This string indicates which errors we own in the editor */
@@ -152,8 +153,8 @@ async function fetchImport({
     console.log('[IMPORTS]', `(${importUrl})`, 'Fetching import');
 
     // Grab the bundle (or bundle of types)
-    const bundle = await fetch(`/api/ts/bundle?x=${importUrl}`).then((r) =>
-      r.json(),
+    const bundle = await fetch(`/api/editor/ts/bundle?x=${importUrl}`).then(
+      (r) => r.json(),
     );
 
     // Add each individual file in the bundle the model
@@ -163,8 +164,10 @@ async function fetchImport({
       const uri = getUriFromPath(
         url,
         uriParser,
-        url.endsWith('tsx') ? 'tsx' : 'ts',
+        isZipperImportUrl(url) || url.endsWith('tsx') ? 'tsx' : 'ts',
       );
+
+      console.log('bundle?', { url, uri, src });
       if (!monacoRef?.current?.editor.getModel(uri)) {
         monacoRef?.current?.editor.createModel(src, 'typescript', uri);
       }
