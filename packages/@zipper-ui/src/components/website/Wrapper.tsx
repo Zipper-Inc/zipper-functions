f54347d7b485facce7e0600f51ffba3c5f4595be
LittiNg_ <littig.works@gmail.com>
2023-08-30 18:52:53 +0000

    feat: adding footer applet + blog mobile otimization

--- packages/@zipper-ui/src/components/website/Wrapper.tsx
+++ packages/@zipper-ui/src/components/website/Wrapper.tsx
@@ -13,24 +13,18 @@ export const Wrapper = ({ children }: { children: ReactNode }) => {
       overflowX="hidden"
     >
       <Box
-        as="span"
-        zIndex={0}
-        w="full"
-        h="2000px"
         position="absolute"
-        top={0}
-        right={'15%'}
-      >
-        <svg
-          width="2000"
-          height="2000"
-          viewBox="0 0 1401 1408"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M-274.352 1408L1401 0H-786V1408H-274.352Z" fill="white" />
-        </svg>
-      </Box>
+        left="0"
+        top="0"
+        w="0"
+        h="0"
+        borderBottom={{
+          base: '600px solid transparent',
+          lg: '1700px solid transparent',
+        }}
+        zIndex={0}
+        borderLeft={{ base: '600px solid white', lg: '1700px solid white' }}
+      />
       <Box as="main" w="full" m="0 auto" zIndex={1} position="relative">
         {children}
       </Box>
