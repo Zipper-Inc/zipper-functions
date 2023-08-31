75a74b07374b7109f3e9187788b0f6eee545f9e7
Sachin Ranchod <sachin@zipper.works>
2023-08-30 19:49:06 -0700

    add new files

---
+++ apps/zipper.dev/src/server/routers/appLog.router.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+import { prisma } from '~/server/prisma';
+import { createRouter } from '../createRouter';
+import { hasAppReadPermission } from '../utils/authz.utils';
+
+export const appLogRouter = createRouter().mutation('get', {
+  input: z.object({
+    appId: z.string(),
+    version: z.string(),
+    runId: z.string().optional(),
+    fromTimestamp: z.number().optional(),
+  }),
+  async resolve({ ctx, input }) {
+    await hasAppReadPermission({
+      ctx,
+      appId: input.appId,
+    });
+
+    const logs = (await prisma.appLog.findMany({
+      where: {
+        ...input,
+      },
+    })) as Zipper.Log.Message[];
+
+    return logs;
+  },
+});
