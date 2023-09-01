a57d26e48000b18234a92fdb23a63d949183a805
Miguel Nascimento <miguelgomes13@live.com>
2023-09-01 16:47:51 -0300

    chore: remove lodash due to phrase.ts => phrase-ts

--- apps/zipper.dev/src/utils/kebab-case.ts
+++ apps/zipper.dev/src/utils/kebab-case.ts
@@ -1,5 +1,5 @@
-import toKebabCase from 'lodash.kebabcase';
-
-// Using lodash because other implementations are not as good
-// See: https://gist.github.com/thevangelist/8ff91bac947018c9f3bfaad6487fa149
-export const kebabCase = toKebabCase;
+export const kebabCase = (str: string) =>
+  str
+    .replace(/([a-z])([A-Z])/g, '$1-$2') // Convert camelCase to kebab-case
+    .replace(/_/g, '-') // Convert snake_case to kebab-case
+    .toLowerCase(); // Ensure all characters are in lowercase
