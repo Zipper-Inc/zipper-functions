d0f354c523d5b16f4aa418cd104e1f86174408ea
imadha <ibu@zipper.works>
2023-10-04 17:37:22 -0500

    Did all that for nothing - was easy to check perms

--- apps/zipper.dev/src/pages/api/liveblocks/auth.ts
+++ apps/zipper.dev/src/pages/api/liveblocks/auth.ts
@@ -1,6 +1,8 @@
 import { authorize } from '@liveblocks/node';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getToken } from 'next-auth/jwt';
+import { createContext } from '~/server/context';
+import { prisma } from '~/server/prisma';
+import { canUserEdit } from '~/server/routers/app.router';
 
 const { LIVEBLOCKS_SECRET_KEY } = process.env;
 
@@ -11,16 +13,30 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     });
   }
 
-  const token = await getToken({ req });
+  const context = await createContext({ req, res });
 
-  if (!token?.sub) {
+  if (!context.userId) {
     return res.status(401).send({ error: 'Unauthorized' });
+  }
+
+  const [, appSlug] = req.body.room.split('/') as [string, string];
+  const applet = await prisma.app.findUnique({
+    where: { slug: appSlug },
+    include: { editors: true },
+  });
+
+  if (!applet) {
+    return res.status(404).send({ error: 'Applet not found' });
+  }
+
+  if (!canUserEdit(applet, context)) {
+    return res.status(403).send({ error: 'Forbidden' });
   }
 
   const response = await authorize({
     room: req.body.room,
     secret: LIVEBLOCKS_SECRET_KEY,
-    userId: token.sub,
+    userId: context.userId,
   });
 
   return res.status(response.status).end(response.body);
