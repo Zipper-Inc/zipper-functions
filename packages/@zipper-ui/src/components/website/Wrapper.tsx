0854cfbd23e3eb2c5a6d11ada2dc593c1f66b06d
LittiNg_ <littig.works@gmail.com>
2023-10-09 21:09:53 -0400

    refactor: hero bg fix for other routes + navbar colors + tablet enhance (#490)

--- packages/@zipper-ui/src/components/website/Wrapper.tsx
+++ packages/@zipper-ui/src/components/website/Wrapper.tsx
@@ -1,7 +1,13 @@
 import { Box, LightMode } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
-export const Wrapper = ({ children }: { children: ReactNode }) => {
+export const Wrapper = ({
+  children,
+  mode = 'light',
+}: {
+  children: ReactNode;
+  mode?: 'dark' | 'light';
+}) => {
   // Force light mode on website stuff for now
   // @todo build out light mode?
   return (
@@ -11,7 +17,11 @@ export const Wrapper = ({ children }: { children: ReactNode }) => {
         as="main"
         m="0 auto"
         position="relative"
-        bgGradient="linear(to-tl, #1789DC, #0766B7)"
+        bg={
+          mode === 'light'
+            ? 'gray.100'
+            : 'linear-gradient(to top left, #1789DC, #0766B7)'
+        }
         overflowY="hidden"
         overflowX="hidden"
       >
@@ -22,22 +32,13 @@ export const Wrapper = ({ children }: { children: ReactNode }) => {
           w="1800px"
           h="1800px"
           clipPath="polygon(0% 0%, 100% 0%, 0% 100%)"
-          bgGradient="linear(to-r, #651D78, #9B2FB4)"
+          bg={
+            mode === 'light'
+              ? 'white'
+              : 'linear-gradient(to right, #651D78, #9B2FB4)'
+          }
           zIndex={0}
         />
-        {/* <Box
-          position="absolute"
-          left="0"
-          top="0"
-          w="0"
-          h="0"
-          borderBottom={{
-            base: '600px solid transparent',
-            lg: '1500px solid transparent',
-          }}
-          zIndex={0}
-          borderLeft={{ base: '600px solid white', lg: '1500px solid white' }}
-        /> */}
         <Box as="main" w="full" m="0 auto" zIndex={1} position="relative">
           {children}
         </Box>
