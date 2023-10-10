e15cc559be7aa88d3a895585bf9e4baaa0ca8eeb
imadha <ibu@zipper.works>
2023-10-10 13:16:55 -0500

    Better seperation for future needs

--- apps/zipper.dev/src/utils/rewrite-imports.ts
+++ apps/zipper.dev/src/utils/rewrite-imports.ts
@@ -1,4 +1,4 @@
-import { SourceFile } from 'ts-morph';
+import type { SourceFile } from 'ts-morph';
 /** This has to use the server version or build may break */
 import { getZipperDotDevUrlForServer } from '~/server/utils/server-url.utils';
 import { getSourceFileFromCode, isExternalImport } from './parse-code';
@@ -6,32 +6,53 @@ import { getSourceFileFromCode, isExternalImport } from './parse-code';
 const DEFAULT_NPM_CDN = 'https://esm.sh';
 const withNpmCdn = (specifier: string) => `${DEFAULT_NPM_CDN}/${specifier}`;
 
-export function rewriteSpecifier(specifier: string) {
-  // Don't touch file imports or urls
+export enum RewriteTo {
+  None,
+  ZipperDotDev,
+  NpmCdn,
+}
+
+/**
+ * Rewrite certain imports to enable special Zipper use cases
+ *
+ * | Specifier | Rewrite To |
+ * | --------- | ----------|
+ * | ./whatever.ts | ./whatever.ts |
+ * | https://deno.land/x/whatever/mod.ts | https://deno.land/x/whatever/mod.ts |
+ * | /hourglass-inc/hr-utils/src/main.ts | https://zipper.dev/hourglass-inc/hr-utils/src/main.ts |
+ * | npm:lodash | https://esm.sh/lodash |
+ * | node:fs | https://esm.sh/fs |
+ * | lodash | https://esm.sh/lodash |
+ */
+export function getRewriteRule(specifier: string): RewriteTo {
   if (
     specifier.startsWith('./') ||
     specifier.startsWith('../') ||
     isExternalImport(specifier)
   ) {
-    return specifier;
+    return RewriteTo.None;
   }
 
-  // Treat the root like the root of zipper.dev
-  if (specifier.startsWith('/')) {
-    return `${getZipperDotDevUrlForServer()}${specifier.replace(/^\//, '')}`;
+  if (specifier.startsWith('/')) return RewriteTo.ZipperDotDev;
+  if (specifier.startsWith('npm:')) return RewriteTo.NpmCdn;
+  if (specifier.startsWith('node:')) return RewriteTo.NpmCdn;
+
+  // default to npm
+  return RewriteTo.NpmCdn;
+}
+
+export function rewriteSpecifier(specifier: string) {
+  switch (getRewriteRule(specifier)) {
+    case RewriteTo.None:
+      return specifier;
+
+    case RewriteTo.ZipperDotDev:
+      return `${getZipperDotDevUrlForServer()}${specifier.replace(/^\//, '')}`;
+
+    case RewriteTo.NpmCdn:
+    default:
+      return withNpmCdn(specifier.replace(/^(npm|node):/, ''));
   }
-
-  // Fix the npm ones
-  if (specifier.startsWith('npm:')) {
-    return withNpmCdn(specifier.replace('npm:', ''));
-  }
-
-  /** @todo */
-  // handle `node:` imports
-  // Maybe we can just use the CDN for those too?
-
-  // Assume everthing else is from npm
-  return withNpmCdn(specifier);
 }
 
 export function rewriteImports(src?: string | SourceFile) {
