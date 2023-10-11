174c0649eaae1ed187261f87e670286f27f0859b
Sachin Ranchod <sachin@zipper.works>
2023-10-10 22:27:32 -0700

    updates from Alvin (#493)

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,19 +1,12 @@
-import { useColorMode } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
-import { withDefaultTheme } from '@zipper/ui';
-import { useEffect } from 'react';
-import { useTheme } from 'next-themes';
 
 function MyApp(props: AppProps) {
-  const { setColorMode: setChakraColorMode } = useColorMode();
-  const { theme: nextThemesTheme } = useTheme();
-
-  // Make sure the `next-themes` tailwind docs theme is in sync with the rest of chakra stuff
-  useEffect(() => {
-    setChakraColorMode(nextThemesTheme);
-  }, [nextThemesTheme]);
-
-  return <props.Component {...props.pageProps} />;
+  return (
+    <ChakraProvider>
+      <props.Component {...props.pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default withDefaultTheme(MyApp);
+export default MyApp;
