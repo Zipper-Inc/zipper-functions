dcd4cc8f5e1724dc0b6223fcaafd6ff410c9d0f5
imadha <ibu@zipper.works>
2023-10-19 22:38:07 -0500

    Make config TS

---
+++ apps/zipper.dev/src/liveblocks.config.ts
@@ -0,0 +1,59 @@
+import { createClient, LiveObject } from '@liveblocks/client';
+import { createRoomContext } from '@liveblocks/react';
+import { type Selection as MonacoSelection } from 'monaco-editor';
+import type LiveblocksProvider from '@liveblocks/yjs';
+
+const client = createClient({
+  authEndpoint: '/api/liveblocks/auth',
+});
+
+export const getSelectionObject = (s: MonacoSelection) => ({ ...s });
+export type Selection = ReturnType<typeof getSelectionObject>;
+
+export type Presence = {
+  selection?: ReturnType<typeof getSelectionObject>;
+};
+
+export type StoredScriptId = `script-${string}`;
+export type Storage = {
+  app: LiveObject<{ slug: string; name: string; description: string }>;
+  [scriptId: StoredScriptId]: LiveObject<{ code: string }>;
+};
+
+export type UserMeta = {
+  id: string;
+};
+
+export type UserAwareness = {
+  user?: UserMeta['id'];
+};
+
+export type AwarenessList = [number, UserAwareness][];
+
+// Optionally, the type of custom events broadcast and listened to in this
+// room. Use a union for multiple events. Must be JSON-serializable.
+export type RoomEvent = {
+  // type: "NOTIFICATION",
+  // ...
+};
+
+export type TypedLiveblocksProvider = LiveblocksProvider<
+  Presence,
+  Storage,
+  UserMeta,
+  RoomEvent
+>;
+
+export const {
+  suspense: {
+    useStorage,
+    RoomProvider,
+    useMutation,
+    useOther,
+    useOthers,
+    useOthersConnectionIds,
+    useSelf,
+    useMyPresence,
+    useRoom,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent>(client);
