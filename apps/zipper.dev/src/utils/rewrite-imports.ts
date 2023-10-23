7659431684785cafafc0ab60a72d274db3bc4c26
imadha <ibu@zipper.works>
2023-10-23 18:38:44 -0500

    Target deno by default

--- apps/zipper.dev/src/utils/rewrite-imports.ts
+++ apps/zipper.dev/src/utils/rewrite-imports.ts
@@ -3,15 +3,35 @@ import type { SourceFile } from 'ts-morph';
 import { getZipperDotDevUrlForServer } from '~/server/utils/server-url.utils';
 import { getSourceFileFromCode, isExternalImport } from './parse-code';
 
-const DEFAULT_NPM_CDN = 'https://esm.sh';
-const withNpmCdn = (specifier: string) => `${DEFAULT_NPM_CDN}/${specifier}`;
+const ESM_SH_ORIGN = 'https://esm.sh';
+const SKYPACK_ORIGIN = 'https://cdn.skypack.dev';
+
+const withDenoTarget = (specifier: string) => {
+  try {
+    const url = new URL(specifier);
+    if (url.origin === ESM_SH_ORIGN && !url.searchParams.has('target'))
+      url.searchParams.set('target', 'deno');
+
+    if (url.origin === SKYPACK_ORIGIN && !url.searchParams.has('dts'))
+      url.searchParams.set('dts', '1');
+
+    return url.toString();
+  } catch (e) {
+    return specifier;
+  }
+};
+
+const withEsmSh = (specifier: string) =>
+  withDenoTarget(`${ESM_SH_ORIGN}/${specifier}`);
+
 export const LOCALHOST_URL_REGEX =
   /^(?:https?:\/\/)(?:localhost|127\.0\.0\.1|10\.(?:\d{1,3}\.){2}\d{1,3}|172\.(?:1[6-9]|2\d|3[01])\.(?:\d{1,3}\.){2}\d{1,3}|192\.168\.(?:\d{1,3}\.){1}\d{1,3}|::1)(?:\:\d+)?/;
 
 export enum RewriteTo {
   None,
   ZipperDotDev,
-  NpmCdn,
+  EsmSh,
+  External,
 }
 
 /**
@@ -22,9 +42,9 @@ export enum RewriteTo {
  * | ./whatever.ts | ./whatever.ts |
  * | https://deno.land/x/whatever/mod.ts | https://deno.land/x/whatever/mod.ts |
  * | /hourglass-inc/hr-utils/src/main.ts | https://zipper.dev/hourglass-inc/hr-utils/src/main.ts |
- * | npm:lodash | https://esm.sh/lodash |
- * | node:fs | https://esm.sh/fs |
- * | lodash | https://esm.sh/lodash |
+ * | npm:lodash | https://esm.sh/lodash?target=deno |
+ * | node:fs | https://esm.sh/fs?target=deno |
+ * | lodash | https://esm.sh/lodash?target=deno |
  */
 export function getRewriteRule(specifier: string): RewriteTo {
   // Someone clever might try to sniff out the server URL
@@ -33,21 +53,20 @@ export function getRewriteRule(specifier: string): RewriteTo {
     return RewriteTo.ZipperDotDev;
   }
 
-  if (
-    !specifier ||
-    specifier.startsWith('./') ||
-    specifier.startsWith('../') ||
-    isExternalImport(specifier)
-  ) {
+  if (!specifier || specifier.startsWith('./') || specifier.startsWith('../')) {
     return RewriteTo.None;
   }
 
+  if (isExternalImport(specifier)) {
+    return RewriteTo.External;
+  }
+
   if (specifier.startsWith('npm:') || specifier.startsWith('node:')) {
-    return RewriteTo.NpmCdn;
+    return RewriteTo.EsmSh;
   }
 
   // default to npm
-  return RewriteTo.NpmCdn;
+  return RewriteTo.EsmSh;
 }
 
 export function rewriteSpecifier(
@@ -58,14 +77,17 @@ export function rewriteSpecifier(
     case RewriteTo.None:
       return specifier;
 
+    case RewriteTo.External:
+      return withDenoTarget(specifier);
+
     case RewriteTo.ZipperDotDev:
       return `${getZipperDotDevUrlForServer()}${specifier
         .replace(LOCALHOST_URL_REGEX, '')
         .replace(/^\//, '')}`;
 
-    case RewriteTo.NpmCdn:
+    case RewriteTo.EsmSh:
     default:
-      return withNpmCdn(specifier.replace(/^(npm|node):/, ''));
+      return withEsmSh(specifier.replace(/^(npm|node):/, ''));
   }
 }
 
