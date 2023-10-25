6be9df8cd7d64b6183c50839bbfafc83b2264b0b
Sachin Ranchod <sachin@zipper.works>
2023-10-24 23:57:41 -0700

    only show buttons on hover

--- apps/zipper.dev/src/components/gallery/gallery-item.tsx
+++ apps/zipper.dev/src/components/gallery/gallery-item.tsx
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react';
 import AppAvatar from '../app-avatar';
 import { RouterOutputs } from '~/utils/trpc';
+import { useState } from 'react';
 
 type GalleryItemProps = {
   app: Unpack<GalleryAppQueryOutput>;
@@ -20,13 +21,84 @@ export type GalleryAppQueryOutput =
   | RouterOutputs['app']['allApproved']
   | RouterOutputs['app']['byResourceOwner'];
 
+const InnerCard: React.FC<GalleryItemProps> = ({ app }) => {
+  const nameOrSlug = app.name || app.slug;
+  const [isHovering, setIsHovering] = useState(false);
+  return (
+    <CardBody
+      padding={0}
+      as={HStack}
+      alignItems="start"
+      justifyContent="stretch"
+      p={6}
+      spacing={4}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <Box w={24}>
+        <AppAvatar nameOrSlug={nameOrSlug} />
+      </Box>
+      <VStack alignItems="start" spacing={8} flex={1}>
+        <VStack alignItems="start" spacing="-0.5" flex={1}>
+          <Text as="h2" fontSize="lg" fontWeight="semibold">
+            {nameOrSlug}{' '}
+          </Text>
+          <HStack>
+            <Text as="span" fontSize="md" fontStyle="italic" fontWeight="light">
+              by
+            </Text>
+            <Text as="span" fontSize="md" fontWeight="normal">
+              {app.resourceOwner.slug}
+            </Text>
+            {app.isPrivate && (
+              <Badge
+                colorScheme="blackAlpha"
+                paddingX={2}
+                color="fg.600"
+                ml="2"
+              >
+                Private
+              </Badge>
+            )}
+          </HStack>
+          {isHovering ? (
+            <HStack spacing={2} pt="3">
+              <Button variant="outline" colorScheme="purple" size="sm">
+                <Link href={`/${app.resourceOwner.slug}/${app.slug}`}>
+                  Open Applet
+                </Link>
+              </Button>
+              <Button variant="link" colorScheme="purple" size="sm">
+                <Link href={`/gallery/${app.resourceOwner.slug}/${app.slug}`}>
+                  Learn more
+                </Link>
+              </Button>
+            </HStack>
+          ) : (
+            <Text
+              pt="2"
+              color="fg.500"
+              fontSize="sm"
+              isTruncated
+              noOfLines={2}
+              whiteSpace="pre-line"
+            >
+              {app.description}
+            </Text>
+          )}
+        </VStack>
+      </VStack>
+    </CardBody>
+  );
+};
+
 export const GalleryItem: React.FC<GalleryItemProps> = ({ app }) => {
   if (!app) return <></>;
-  const nameOrSlug = app.name || app.slug;
 
   return (
     <Box>
       <Card
+        as={Link}
         background="bgColor"
         flexGrow={1}
         overflow="hidden"
@@ -35,71 +107,9 @@ export const GalleryItem: React.FC<GalleryItemProps> = ({ app }) => {
         boxShadow="none"
         borderWidth="1px"
         borderColor="fg.200"
+        href={`/gallery/${app.resourceOwner.slug}/${app.slug}`}
       >
-        <CardBody
-          padding={0}
-          as={HStack}
-          alignItems="start"
-          justifyContent="stretch"
-          p={6}
-          spacing={4}
-        >
-          <Box w={24}>
-            <AppAvatar nameOrSlug={nameOrSlug} />
-          </Box>
-          <VStack alignItems="start" spacing={8} flex={1}>
-            <VStack alignItems="start" spacing="-0.5" flex={1}>
-              <Text as="h2" fontSize="lg" fontWeight="semibold">
-                {nameOrSlug}{' '}
-              </Text>
-              <HStack>
-                <Text
-                  as="span"
-                  fontSize="md"
-                  fontStyle="italic"
-                  fontWeight="light"
-                >
-                  by
-                </Text>
-                <Text as="span" fontSize="md" fontWeight="normal">
-                  {app.resourceOwner.slug}
-                </Text>
-                {app.isPrivate && (
-                  <Badge
-                    colorScheme="blackAlpha"
-                    paddingX={2}
-                    color="fg.600"
-                    ml="2"
-                  >
-                    Private
-                  </Badge>
-                )}
-              </HStack>
-              <Text
-                pt="2"
-                color="fg.500"
-                fontSize="sm"
-                isTruncated
-                noOfLines={2}
-                whiteSpace="pre-line"
-              >
-                {app.description}
-              </Text>
-            </VStack>
-            <HStack spacing={4}>
-              <Button variant="outline" colorScheme="purple">
-                <Link href={`/${app.resourceOwner.slug}/${app.slug}`}>
-                  Open Applet
-                </Link>
-              </Button>
-              <Button variant="ghost" colorScheme="purple">
-                <Link href={`/gallery/${app.resourceOwner.slug}/${app.slug}`}>
-                  Learn more
-                </Link>
-              </Button>
-            </HStack>
-          </VStack>
-        </CardBody>
+        <InnerCard app={app}></InnerCard>
       </Card>
     </Box>
   );
