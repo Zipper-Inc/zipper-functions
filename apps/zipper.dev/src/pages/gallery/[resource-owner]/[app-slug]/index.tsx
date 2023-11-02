c5104cea404a872627be1b99eb1ffab9dfdaf916
Sachin Ranchod <sachin@zipper.works>
2023-11-01 23:56:07 -0700

    add clerk blogpost; hackfix embedded auto running applets

--- apps/zipper.dev/src/pages/gallery/[resource-owner]/[app-slug]/index.tsx
+++ apps/zipper.dev/src/pages/gallery/[resource-owner]/[app-slug]/index.tsx
@@ -152,7 +152,7 @@ const AppletLandingPage: NextPageWithLayout = () => {
           alignContent="start"
           alignItems="start"
         >
-          <VStack align="start" minW={400}>
+          <VStack align="start" minW={400} maxW={1000}>
             <AppletAuthor
               author={{
                 name: data.appAuthor.name,
