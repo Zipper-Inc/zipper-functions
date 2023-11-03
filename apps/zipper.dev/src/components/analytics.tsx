4f08ce7337e17735636c77bbb8fac6692ef8cbc0
Sachin Ranchod <sachin@zipper.works>
2023-11-03 13:18:16 -0700

    fix google analytics tag

--- apps/zipper.dev/src/components/analytics.tsx
+++ apps/zipper.dev/src/components/analytics.tsx
@@ -1,23 +1,27 @@
-const Analytics = () => (
-  <>
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
+import Head from 'next/head';
+
+const AnalyticsHead = () => {
+  return (
+    <Head>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', ${process.env.GA_MEASUREMENT_ID}, {
+        gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}, {
           page_path: window.location.pathname,
         });
         `,
-      }}
-    />
-  </>
-);
+        }}
+      />
+    </Head>
+  );
+};
 
-export { Analytics };
+export { AnalyticsHead };
