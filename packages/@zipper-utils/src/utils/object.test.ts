a6193a8817b17ef47cd5ade7a8ecaab279342b03
antoniel <antoniel2210@gmail.com>
2023-11-09 14:39:32 -0300

    router push by stringy some values of query string

--- packages/@zipper-utils/src/utils/object.test.ts
+++ packages/@zipper-utils/src/utils/object.test.ts
@@ -2,22 +2,30 @@ import { InputType } from '@zipper/types';
 import { getInputsFromFormData } from './object';
 
 describe('getInputsFromFormData', () => {
-  it('should return the expected inputs from the form data', () => {
+  test('extracts string type input from formData correctly', () => {
     const formData = {
-      input1: 'value1',
-      input2: 'value2',
-      input3: 'value3',
-      input4: 'value4',
+      'worldString:string': 'some',
     };
     const inputParams = [
-      { key: 'input1', type: InputType.string },
-      { key: 'input2', type: InputType.string },
-      { key: 'input3', type: InputType.string },
+      { key: 'worldString', type: 'string' as InputType, optional: false },
     ];
     const expectedInputs = {
-      input1: 'value1',
-      input2: 'value2',
-      input3: 'value3',
+      worldString: 'some',
+    };
+    expect(getInputsFromFormData(formData, inputParams)).toEqual(
+      expectedInputs,
+    );
+  });
+  test('parses and extracts "any" type input as an object from formData', () => {
+    const formData = { 'worldString:any': '{"some":"foo"}' };
+    const inputParams = [
+      { key: 'worldString', type: 'any' as InputType, optional: false },
+    ];
+
+    const expectedInputs = {
+      worldString: {
+        some: 'foo',
+      },
     };
     expect(getInputsFromFormData(formData, inputParams)).toEqual(
       expectedInputs,
