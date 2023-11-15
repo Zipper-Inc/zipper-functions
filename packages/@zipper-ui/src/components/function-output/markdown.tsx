7385b7ffa3b63d1e6f37899e971e60d8e2316875
Sachin Ranchod <sachin@zipper.works>
2023-11-14 16:40:05 -0800

    allow handlebars in markdown (#607)

--- packages/@zipper-ui/src/components/function-output/markdown.tsx
+++ packages/@zipper-ui/src/components/function-output/markdown.tsx
@@ -3,10 +3,38 @@ import remarkGfm from 'remark-gfm';
 import ChakraUIRenderer from '../../utils/chakra-markdown-renderer';
 import React from 'react';
 
-export const Markdown = ({ children }: { children: string }) => (
-  <ReactMarkdown
-    components={ChakraUIRenderer()}
-    children={children}
-    remarkPlugins={[remarkGfm as any]}
-  />
-);
+export const Markdown = ({
+  children,
+  appInfo,
+  currentUser,
+}: {
+  children: string;
+  appInfo?: { slug: string; name: string };
+  currentUser?: { username: string };
+}) => {
+  let content = children;
+  const appMap = {
+    '{{appName}}': 'name',
+    '{{appSlug}}': 'slug',
+  };
+
+  if (appInfo) {
+    content = content.replace(/({{)(appName|appSlug)(}})/gi, (matched) => {
+      return appInfo[
+        appMap[matched as keyof typeof appMap] as keyof typeof appInfo
+      ];
+    });
+  }
+
+  if (currentUser) {
+    content = content.replace(/({{)(username)(}})/gi, currentUser.username);
+  }
+
+  return (
+    <ReactMarkdown
+      components={ChakraUIRenderer()}
+      children={content}
+      remarkPlugins={[remarkGfm as any]}
+    />
+  );
+};
