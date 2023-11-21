bf270111e2e3d9531e6f907b1b06ad756ffacc14
imadha <ibu@zipper.works>
2023-11-21 13:33:07 -0600

    New endpoint to grab deployment info

---
+++ apps/zipper.dev/src/pages/api/bootInfo/deployment/[slug].ts
@@ -0,0 +1,34 @@
+import { formatDeploymentId, type DeploymentParams } from '@zipper/utils';
+import { NextApiHandler } from 'next';
+import { prisma } from '~/server/prisma';
+import { getAppVersionFromHash } from '~/utils/hashing';
+import noop from '~/utils/noop';
+
+const handler: NextApiHandler = async (req, res) => {
+  const result = await prisma.app
+    .findFirst({
+      select: { id: true, publishedVersionHash: true },
+      where: {
+        slug: req.query.slug as string,
+      },
+    })
+    .catch(noop);
+
+  if (!result || !result.id || !result.publishedVersionHash)
+    return res.status(404).json({ ok: false, error: 'Not found', status: 404 });
+
+  const appId = result.id;
+  const version = getAppVersionFromHash(result.publishedVersionHash) || '';
+  const deploymentId = formatDeploymentId({ appId, version });
+
+  return {
+    ok: true,
+    data: {
+      appId,
+      version,
+      deploymentId,
+    },
+  };
+};
+
+export default handler;
