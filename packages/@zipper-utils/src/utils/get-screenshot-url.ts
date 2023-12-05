ecbdc9b05aca37e2fecc4a19e9855bd5e2fa2789
Sachin Ranchod <sachin@zipper.works>
2023-12-04 22:50:24 -0800

    try get a better screenshot (#654)

--- packages/@zipper-utils/src/utils/get-screenshot-url.ts
+++ packages/@zipper-utils/src/utils/get-screenshot-url.ts
@@ -1,4 +1,7 @@
-export function getScreenshotUrl(urlToScreenshot: string): string {
+export function getScreenshotUrl(
+  urlToScreenshot: string,
+  selector?: string,
+): string {
   const imgUrl = new URL(
     process.env.NEXT_PUBLIC_ZIPPER_SCREENSHOTS_URL ||
       'https://screenshots.zipper.run/main.ts',
@@ -6,5 +9,8 @@ export function getScreenshotUrl(urlToScreenshot: string): string {
   imgUrl.searchParams.set('url', urlToScreenshot);
   imgUrl.searchParams.set('format', 'png');
   imgUrl.searchParams.set('thumb_width', '1200');
+  if (selector) {
+    imgUrl.searchParams.set('selector', selector);
+  }
   return imgUrl.toString();
 }
