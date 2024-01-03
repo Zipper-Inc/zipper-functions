fc4bfd600603902db3fe5cc46f4e858b12f5a1de
Miguel Nascimento <miguelgomes13@live.com>
2024-01-03 18:06:08 -0300

    update migration script

--- apps/zipper.dev/src/pages/api/omni/migrateTsTsx/index.ts
+++ apps/zipper.dev/src/pages/api/omni/migrateTsTsx/index.ts
@@ -18,7 +18,9 @@ const renameTsImportsToTsx = async (code: string) => {
     .filter(
       (path) =>
         typeof path.node.source.value === 'string' &&
-        path.node.source.value.endsWith('.ts'),
+        path.node.source.value.startsWith('./') &&
+        path.node.source.value.endsWith('.ts') &&
+        path.node.source.value !== './main.ts',
     )
     .forEach((path) => {
       if (typeof path.node.source.value === 'string') {
@@ -34,29 +36,62 @@ const renameTsImportsToTsx = async (code: string) => {
 export default createOmniApiHandler(async (req, res) => {
   switch (req.method) {
     case HttpMethod.POST:
-      const errors = [];
+      const { limit } = req.body as { limit: number };
 
-      const appletsIds = await prisma.app.findMany({
-        select: {
-          id: true,
+      const errors = [];
+      let totalUpdatedInLimit = 0;
+
+      const totalApplets = await prisma.app.count();
+      const alreadyUpdated = await prisma.appSetting.count({
+        where: { settingName: 'isMigratedToTsx', settingValue: 'true' },
+      });
+
+      const appletsIdsInsideLimit = await prisma.app.findMany({
+        take: Number(limit),
+        where: {
+          settings: {
+            none: {
+              settingName: 'isMigratedToTsx',
+              settingValue: 'true',
+            },
+          },
+        },
+        include: {
+          settings: true,
         },
       });
 
       try {
-        // for each applet
+        // for each applet in the limit
         //   for each script in applet
         //     rename each script filename
         //     update the code with morph
         //     create new playgroundHash based on your new script content and filenames
-        for (const { id: appletId } of appletsIds) {
+        for await (const { id: appletId } of appletsIdsInsideLimit) {
           const updatedScripts: Script[] = [];
 
-          const app = await prisma.app.findUniqueOrThrow({
+          const app = await prisma.app.findFirstOrThrow({
             where: { id: appletId },
             include: { scripts: true },
           });
 
-          // TODO: this block should be a prisma transaction?
+          await prisma.appSetting.upsert({
+            create: {
+              app: { connect: { id: app.id } },
+              settingName: 'isMigratedToTsx',
+              settingValue: 'false',
+            },
+            update: {
+              settingValue: 'false',
+            },
+            where: {
+              settingName_appId: {
+                appId: app.id,
+                settingName: 'isMigratedToTsx',
+              },
+            },
+          });
+
           const nonMainTsScripts = app.scripts.filter(
             (script) =>
               script.filename.endsWith('.ts') && script.filename !== 'main.ts',
@@ -64,13 +99,22 @@ export default createOmniApiHandler(async (req, res) => {
 
           const main = app.scripts.find(
             (script) => script.filename === 'main.ts',
-          )!;
+          );
 
           if (!main) {
             errors.push({ message: 'main.ts not found', appletName: app.name });
+            console.log(
+              'Applet',
+              app.name || 'unknown',
+              app.id,
+              'doesnt have main.ts',
+            );
           }
 
-          for (const script of nonMainTsScripts) {
+          console.log('Updating applet', app.name || 'unknown', app.id);
+
+          for await (const script of nonMainTsScripts) {
+            console.log('- script:', script.filename);
             // rename script that arent main
             const newFilename = script.filename.replace(/\.ts$/, '.tsx');
 
@@ -95,7 +139,8 @@ export default createOmniApiHandler(async (req, res) => {
 
           const hasUpdated = updatedScripts.length > 0;
 
-          if (hasUpdated) {
+          if (hasUpdated && main) {
+            console.log('- script:', 'main.ts');
             // update main code imports
             await renameTsImportsToTsx(main.code)
               .then((newMainCode) =>
@@ -112,31 +157,73 @@ export default createOmniApiHandler(async (req, res) => {
           }
 
           // Rename filename and perform code updates
-          await Promise.all(
-            updatedScripts.map((updatedScript) =>
-              prisma.script.update({
-                where: { id: updatedScript.id },
-                data: {
-                  filename: updatedScript.filename,
-                  code: updatedScript.code,
+          await prisma
+            .$transaction([
+              ...updatedScripts.map((updatedScript) =>
+                prisma.script.update({
+                  where: { id: updatedScript.id },
+                  data: {
+                    filename: updatedScript.filename,
+                    code: updatedScript.code,
+                  },
+                }),
+              ),
+              prisma.appSetting.upsert({
+                create: {
+                  app: { connect: { id: app.id } },
+                  settingName: 'isMigratedToTsx',
+                  settingValue: 'true',
+                },
+                update: {
+                  settingValue: 'true',
+                },
+                where: {
+                  settingName_appId: {
+                    appId: app.id,
+                    settingName: 'isMigratedToTsx',
+                  },
                 },
               }),
-            ),
-          );
-
-          if (hasUpdated) {
-            // get new hash
-            const { hash } = await buildAndStoreApplet({
-              app: { ...app, scripts: updatedScripts },
+            ])
+            .then(() => {
+              totalUpdatedInLimit++;
+              console.log('Updated applet', app.name || 'unknown', app.id);
+            })
+            .catch((e: Error) => {
+              console.log(
+                'Error updating applet',
+                app.name || 'unknown',
+                app.id,
+              );
+              errors.push({
+                appletName: app.name || 'unknown',
+                appletId: app.id,
+                message: e.message,
+              });
             });
 
-            if (hash !== app.playgroundVersionHash) {
-              // update db version hash
-              await prisma.app.update({
-                where: { id: appletId },
-                data: { playgroundVersionHash: hash },
+          if (hasUpdated) {
+            buildAndStoreApplet({
+              app: { ...app, scripts: updatedScripts },
+              userId: 'MIGRATION_SCRIPT',
+            })
+              // get new hash
+              .then(async ({ hash }) => {
+                if (hash !== app.playgroundVersionHash) {
+                  // update db version hash
+                  await prisma.app.update({
+                    where: { id: appletId },
+                    data: { playgroundVersionHash: hash },
+                  });
+                }
+              })
+              .catch((e: Error) => {
+                errors.push({
+                  appletName: app.name || 'unknown',
+                  appletId: app.id,
+                  message: e.message,
+                });
               });
-            }
           }
         }
       } catch (e) {
@@ -154,7 +241,12 @@ export default createOmniApiHandler(async (req, res) => {
             res,
             status: 500,
             body: {
-              data: { errors },
+              data: {
+                errors,
+                totalApplets,
+                totalUpdatedInLimit,
+                totalUpdated: alreadyUpdated + totalUpdatedInLimit,
+              },
               ok: true,
             },
           });
@@ -162,7 +254,14 @@ export default createOmniApiHandler(async (req, res) => {
 
         return successResponse({
           res,
-          body: { data: { ok: true }, ok: true },
+          body: {
+            data: {
+              ok: true,
+              totalApplets,
+              totalUpdated: alreadyUpdated + totalUpdatedInLimit,
+            },
+            ok: true,
+          },
         });
       }
 
