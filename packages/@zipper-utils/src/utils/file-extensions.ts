456a3ad9c1d4e9ae5f58595a55610d88cef61bf9
Miguel Nascimento <miguelgomes13@live.com>
2024-01-10 00:08:07 -0300

    improvements on file extension utils

--- packages/@zipper-utils/src/utils/file-extensions.ts
+++ packages/@zipper-utils/src/utils/file-extensions.ts
@@ -1,7 +1,20 @@
+import { z } from 'zod';
+
 export const getFileExtension = (filename: string) => {
+  if (!filename.includes('.')) return;
   return filename.split('.').pop();
 };
 
 export const removeExtension = (filename: string) => {
+  if (!getFileExtension(filename)) return filename;
   return filename.split('.').slice(0, -1).join('.');
+};
+export const RunnableExtensionSchema = z.enum(['ts', 'tsx']);
+
+type RunnableExtension = z.infer<typeof RunnableExtensionSchema>;
+
+export const isRunnableExtension = (
+  extension: string,
+): extension is RunnableExtension => {
+  return RunnableExtensionSchema.safeParse(extension).success;
 };
