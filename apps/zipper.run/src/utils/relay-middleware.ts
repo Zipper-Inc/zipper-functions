c7e07c53676731b467ce8f4a6d0ad3a2808b5667
Sachin Ranchod <sachin@zipper.works>
2024-01-16 18:15:20 +0000

    optionally render big results inline; fix share modal button colors; clarify that data is redacted when sensitive mode is on

--- apps/zipper.run/src/utils/relay-middleware.ts
+++ apps/zipper.run/src/utils/relay-middleware.ts
@@ -32,7 +32,8 @@ const DEPLOY_KID = 'zipper';
 const DENO_ORIGIN = new URL(`https://subhosting-v1.deno-aws.net`);
 const RPC_ROOT = `https://${PUBLICLY_ACCESSIBLE_RPC_HOST}/api/deno/v0/`;
 
-const SENSITIVE_DATA_PLACEHOLDER = '********';
+const SENSITIVE_DATA_PLACEHOLDER =
+  'The result of this run was redacted. This behavior can be changed in the app settings.';
 
 const X_FORWARDED_HOST = 'x-forwarded-host';
 const X_DENO_SUBHOST = 'x-deno-subhost';
