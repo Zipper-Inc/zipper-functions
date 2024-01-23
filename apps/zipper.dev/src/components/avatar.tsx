6279e26331a5fbcd16d452f5e4380bb4fc62dcd8
Matheus Littig <littig.works@gmail.com>
2024-01-23 12:42:29 -0400

    Hot-fix: avatars playground (now true) (#739)
    * fix: rolling back to chakra avatars
    
    * fix: rolling back to chakra avatars

--- apps/zipper.dev/src/components/avatar.tsx
+++ apps/zipper.dev/src/components/avatar.tsx
@@ -1,6 +1,5 @@
 import { trpc } from '~/utils/trpc';
 import { Avatar as BaseAvatar, AvatarProps } from '@chakra-ui/react';
-import { Avatar as ShadAvatar } from '@zipper/ui';
 import { useUser } from '~/hooks/use-user';
 
 export function AvatarForUserId({
@@ -8,47 +7,47 @@ export function AvatarForUserId({
   ...props
 }: { userId: string } & AvatarProps) {
   const userQuery = trpc.user.profileForUserId.useQuery({ id: userId });
-  // return (
-  //   <BaseAvatar
-  //     name={userQuery?.data?.name || ''}
-  //     src={userQuery?.data?.image || undefined}
-  //     {...props}
-  //   />
-  // );
-
   return (
-    <ShadAvatar>
-      <ShadAvatar.Image
-        src={userQuery.data?.image as string}
-        alt={userQuery.data?.name as string}
-      />
-      <ShadAvatar.Fallback>
-        {userQuery.data?.name?.charAt(0)}
-      </ShadAvatar.Fallback>
-    </ShadAvatar>
+    <BaseAvatar
+      name={userQuery?.data?.name || ''}
+      src={userQuery?.data?.image || undefined}
+      {...props}
+    />
   );
+
+  // return (
+  //   <ShadAvatar>
+  //     <ShadAvatar.Image
+  //       src={userQuery.data?.image as string}
+  //       alt={userQuery.data?.name as string}
+  //     />
+  //     <ShadAvatar.Fallback>
+  //       {userQuery.data?.name?.charAt(0)}
+  //     </ShadAvatar.Fallback>
+  //   </ShadAvatar>
+  // );
 }
 
 export function AvatarForCurrentUser(props: AvatarProps) {
   const { user } = useUser();
-  // return (
-  //   <BaseAvatar
-  //     name={user?.name || ''}
-  //     referrerPolicy="no-referrer"
-  //     {...props}
-  //     src={user?.image || ''}
-  //   />
-  // );
-
   return (
-    <ShadAvatar>
-      <ShadAvatar.Image
-        src={user?.image as string}
-        alt={user?.name as string}
-      />
-      <ShadAvatar.Fallback>{user?.image}</ShadAvatar.Fallback>
-    </ShadAvatar>
+    <BaseAvatar
+      name={user?.name || ''}
+      referrerPolicy="no-referrer"
+      {...props}
+      src={user?.image || ''}
+    />
   );
+
+  // return (
+  //   <ShadAvatar>
+  //     <ShadAvatar.Image
+  //       src={user?.image as string}
+  //       alt={user?.name as string}
+  //     />
+  //     <ShadAvatar.Fallback>{user?.image}</ShadAvatar.Fallback>
+  //   </ShadAvatar>
+  // );
 }
 
 export function Avatar({
