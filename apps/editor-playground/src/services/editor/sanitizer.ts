7fc0e8cc631cae12b11b99ad9597a41d2c89db64
Miguel Nascimento <miguelgomes13@live.com>
2024-02-08 17:20:55 -0300

    chore: allow filename with length less than 3

--- apps/editor-playground/src/services/editor/sanitizer.ts
+++ apps/editor-playground/src/services/editor/sanitizer.ts
@@ -1,17 +1,18 @@
 export const sanitizer = {
   filename: (filename: string) => {
-    if (filename.length < 3) {
-      throw new Error('Invalid File name: filenames must be at least 4 chars.');
-    }
-    
-    if (!['.ts', '.md'].includes(filename.slice(-3)) && filename.includes('.')) {
-      throw new Error('Invalid File extension: only .ts and .md files allowed.');
-    }
-
-    if (['.ts', '.md'].includes(filename.slice(-3))){
-      return filename
+    if (
+      !['.ts', '.md'].includes(filename.slice(-3)) &&
+      filename.includes('.')
+    ) {
+      throw new Error(
+        'Invalid File extension: only .ts and .md files allowed.',
+      );
     }
 
-    return filename + '.ts'
+    if (['.ts', '.md'].includes(filename.slice(-3))) {
+      return filename;
+    }
+
+    return filename + '.ts';
   },
 };
